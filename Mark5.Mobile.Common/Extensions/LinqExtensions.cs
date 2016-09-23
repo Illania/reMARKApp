ed47f43190eccccf93d5a712f11c1ec83646f8d0
Ferdinando Papale <fp@nordic-it.com>
2016-09-23 11:02:27 +0200

    Created flatten extension method

--- Mark5.Mobile.Common/Extensions/LinqExtensions.cs
+++ Mark5.Mobile.Common/Extensions/LinqExtensions.cs
@@ -5,6 +5,7 @@
 //
 // Copyright (c) 2016 Nordic IT
 //
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,10 +14,14 @@ namespace Mark5.Mobile.Common.Extensions
 
     public static class LinqExtensions
     {
-
         public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T> enumerable) where T : class
         {
             return enumerable.Where(t => t != null);
+        }
+
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<T> e, Func<T, IEnumerable<T>> childSelector)
+        {
+            return e.SelectMany(childSelector).Concat(e);
         }
     }
 }
