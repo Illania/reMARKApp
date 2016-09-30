f29df6d5e2d31e0c6fa1f6af06d564aedea0f61f
Ferdinando Papale <fp@nordic-it.com>
2016-09-30 15:02:12 +0200

    Added icons

---
+++ Mark5.Mobile.Droid/Ui/Views/PaddingDivider.cs
@@ -0,0 +1,17 @@
+﻿//
+// Project: 
+// File: PaddingDivider.cs
+// Author: Ferdinando Papale fp@nordic-it.com
+//
+// Copyright (c) 2016 Nordic IT
+//
+using System;
+namespace Mark5.Mobile.Droid
+{
+    public class PaddingDivider
+    {
+        public PaddingDivider()
+        {
+        }
+    }
+}
