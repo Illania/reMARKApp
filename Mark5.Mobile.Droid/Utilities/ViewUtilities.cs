8af98037f27a75bfd1854ce2044675875bb9ee49
Ferdinando Papale <fp@nordic-it.com>
2016-09-30 16:17:34 +0200

    Added view utilities and style

---
+++ Mark5.Mobile.Droid/Utilities/ViewUtilities.cs
@@ -0,0 +1,33 @@
+﻿//
+// Project: 
+// File: ViewUtilities.cs
+// Author: Ferdinando Papale fp@nordic-it.com
+//
+// Copyright (c) 2016 Nordic IT
+//
+using System;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace Mark5.Mobile.Droid.Utilities
+{
+    public static class ViewUtilities
+    {
+        public static void SetTextAppearanceCompat(this TextView view, Context context, int resourceId)
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.M)
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                view.SetTextAppearance(context, resourceId);
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
+            else
+            {
+#pragma warning disable XA0001 // Find issues with Android API usage
+                view.SetTextAppearance(resourceId);
+#pragma warning restore XA0001 // Find issues with Android API usage
+            }
+        }
+    }
+}
