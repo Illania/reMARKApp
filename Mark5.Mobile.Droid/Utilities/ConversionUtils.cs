517e5ce9e40da2ffeda0f20ba3e1f951c44a7030
Ferdinando Papale <fp@nordic-it.com>
2016-10-11 11:38:13 +0200

    Added conversion utils

---
+++ Mark5.Mobile.Droid/Utilities/ConversionUtils.cs
@@ -0,0 +1,20 @@
+﻿//
+// Project: 
+// File: ConversionUtils.cs
+// Author: Ferdinando Papale fp@nordic-it.com
+//
+// Copyright (c) 2016 Nordic IT
+//
+using Android.Util;
+
+namespace Mark5.Mobile.Droid.Utilities
+{
+    public static class ConversionUtils
+    {
+        public static int ConvertDpToPixels(int dp)
+        {
+            return (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, dp, Android.App.Application.Context.Resources.DisplayMetrics);
+        }
+    }
+}
+
