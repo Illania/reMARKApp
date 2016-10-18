89554b9f40ad42b2e833938d26314d7afcdd3fc3
Bartosz Cichecki <bgc@nordic-it.com>
2016-10-18 17:16:46 +0200

    Initial implementation.

---
+++ Mark5.Mobile.Droid/Ui/Activities/ObjectLinksActivity.cs
@@ -0,0 +1,76 @@
+﻿//
+// Project: Mark5.Mobile.Droid
+// File: ObjectLinksActivity.cs
+// Author: Bartosz Cichecki <bgc@nordic-it.com>
+//
+// Copyright (c) 2016 Nordic IT
+//
+using System;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Common.Utilities;
+using Mark5.Mobile.Droid.Ui.Common;
+using Mark5.Mobile.Droid.Ui.Fragments;
+
+namespace Mark5.Mobile.Droid.Ui.Activities
+{
+
+    [Activity]
+    public class ObjectLinksActivity : BaseAppCompatActivity
+    {
+
+        public const string BusinessEntityIntentKey = "BusinessEntity_ef8f3886-1478-4b4c-8bdb-7a6188035674";
+        public const string BusinessEntityTypeIntentKey = "BusinessEntityType_5763e14a-b99d-4bdd-9cec-f54cfaf17ae0";
+
+        Toolbar toolbar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            CommonConfig.Logger.Info($"Creating {nameof(ObjectLinksActivity)}...");
+
+            SetTitle(Resource.String.links);
+            SetContentView(Resource.Layout.base_layout);
+
+            toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+            if (savedInstanceState == null)
+            {
+                var type = SerializationUtils.Deserialize<Type>(Intent.Extras.GetString(BusinessEntityTypeIntentKey));
+                var be = SerializationUtils.Deserialize(Intent.Extras.GetString(BusinessEntityIntentKey), type) as IBusinessEntity;
+                var ft = SupportFragmentManager.BeginTransaction();
+                var dlf = new ObjectLinksFragment
+                {
+                    BusinessEntity = be
+                };
+                ft.Replace(Resource.Id.fragment_container, dlf, dlf.GenerateTag());
+                ft.Commit();
+
+                CommonConfig.Logger.Info($"Created {nameof(ObjectLinksActivity)}");
+            }
+            else
+            {
+                CommonConfig.Logger.Info($"Restored {nameof(ObjectLinksActivity)}");
+            }
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item.ItemId == Android.Resource.Id.Home)
+            {
+                OnBackPressed();
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
---
+++ Mark5.Mobile.Droid/Ui/Fragments/ObjectLinksFragment.cs
@@ -0,0 +1,149 @@
+﻿//
+// Project: Mark5.Mobile.Droid
+// File: ObjectLinksFragment.cs
+// Author: Bartosz Cichecki <bgc@nordic-it.com>
+//
+// Copyright (c) 2016 Nordic IT
+//
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Android.Graphics;
+using Android.OS;
+using Android.Support.V4.Content;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Managers;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Droid.Ui.Common;
+using Mark5.Mobile.Droid.Ui.Views;
+using System.Linq;
+
+namespace Mark5.Mobile.Droid.Ui.Fragments
+{
+
+    public class ObjectLinksFragment : RetainableStateFragment
+    {
+
+        public IBusinessEntity BusinessEntity
+        {
+            get;
+            set;
+        }
+
+        public Action CloseRequest { get; set; }
+
+        List<ObjectLink> objectLinks;
+
+        ProgressBar progress;
+        ScrollView scrollView;
+        LinearLayoutCompat linearLayout;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            CommonConfig.Logger.Info($"Creating {nameof(ObjectLinksFragment)} [businessEntity.id={BusinessEntity.Id}, businessEntity.objectType={BusinessEntity.ObjectType}]...");
+
+            var rootView = inflater.Inflate(Resource.Layout.linear_layout, container, false);
+            rootView.SetBackgroundColor(new Color(ContextCompat.GetColor(Context, Resource.Color.lightgray)));
+
+            progress = rootView.FindViewById<ProgressBar>(Resource.Id.progress);
+            scrollView = rootView.FindViewById<ScrollView>(Resource.Id.scroll_view);
+            linearLayout = rootView.FindViewById<LinearLayoutCompat>(Resource.Id.linear_layout);
+            var padding = (int)(TypedValue.ApplyDimension(ComplexUnitType.Dip, 10.0f, Resources.DisplayMetrics) + 0.5f);
+            linearLayout.SetPadding(padding, padding, padding, padding);
+
+            return rootView;
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+
+            ((AppCompatActivity)Activity).SupportActionBar.Title = GetString(Resource.String.links);
+
+            CommonConfig.Logger.Info($"Created {nameof(ObjectLinksFragment)} [businessEntity.id={BusinessEntity.Id}, businessEntity.objectType={BusinessEntity.ObjectType}]...");
+        }
+
+        public override async void OnResume()
+        {
+            base.OnResume();
+
+            await RefreshData();
+        }
+
+        async Task RefreshData()
+        {
+            try
+            {
+                if (objectLinks == null)
+                {
+                    objectLinks = await Managers.CommonActionsManager.GetObjectLinksAsync(BusinessEntity);
+                }
+
+                RefreshView();
+            }
+            catch (Exception ex)
+            {
+                CommonConfig.Logger.Error($"Downloading object actions failed [businessEntity.id={BusinessEntity.Id}, businessEntity.objectType={BusinessEntity.ObjectType}]", ex);
+
+                await Dialogs.ShowErrorDialogAsync(Activity, ex);
+
+                if (CloseRequest != null) CloseRequest();
+            }
+        }
+
+        void RefreshView()
+        {
+            progress.Visibility = ViewStates.Gone;
+            scrollView.Visibility = ViewStates.Visible;
+
+            linearLayout.RemoveViews(0, linearLayout.ChildCount);
+
+            var grouppedObjectLinks = objectLinks.OrderBy(oa => oa.TypeInfo.DescriptionAction).GroupBy(oa => oa.TypeInfo.DescriptionAction);
+
+            foreach (var grouppedObjectLink in grouppedObjectLinks)
+            {
+                linearLayout.AddView(new ObjectLinksView(Context, grouppedObjectLink.Key, grouppedObjectLink.ToArray()));
+            }
+
+            linearLayout.Invalidate();
+            linearLayout.RequestLayout();
+        }
+
+        public override IRetainableState OnRetainInstanceState()
+        {
+            return new ObjectLinksFragmentState
+            {
+                BusinessEntity = BusinessEntity,
+                ObjectLinks = objectLinks
+            };
+        }
+
+        public override void OnRetainedInstanceStateRestored(IRetainableState restoredState)
+        {
+            var oafs = restoredState as ObjectLinksFragmentState;
+            if (oafs != null)
+            {
+                BusinessEntity = oafs.BusinessEntity;
+                objectLinks = oafs.ObjectLinks;
+            }
+        }
+
+        public override string GenerateTag()
+        {
+            return $"{nameof(ObjectLinksFragment)} [businessEntity.id={BusinessEntity.Id}, businessEntity.objectType={BusinessEntity.ObjectType}]";
+        }
+
+        class ObjectLinksFragmentState : IRetainableState
+        {
+
+            public IBusinessEntity BusinessEntity { get; set; }
+
+            public List<ObjectLink> ObjectLinks { get; set; }
+        }
+    }
+}
