57c7a7aee78794e702389681c358dffccf8ad97c
Bartosz Cichecki <bgc@nordic-it.com>
2016-10-26 10:36:48 +0200

    Corrections to date range picker

--- Mark5.Mobile.Droid/Ui/Views/DocumentsSearchViews/DateRangeSearchView.cs
+++ Mark5.Mobile.Droid/Ui/Views/DocumentsSearchViews/DateRangeSearchView.cs
@@ -5,10 +5,11 @@
 //
 // Copyright (c) 2016 Nordic IT
 //
+using System;
 using Android.Content;
 using Android.Support.V7.Widget;
+using Android.Text.Format;
 using Android.Views;
-using Android.Widget;
 using Mark5.Mobile.Common.Model;
 using Mark5.Mobile.Droid.Ui.Common;
 using Mark5.Mobile.Droid.Utilities;
@@ -24,6 +25,11 @@ namespace Mark5.Mobile.Droid.Ui.Views.DocumentsSearchViews
         readonly AppCompatTextView dateRangeFrom;
         readonly AppCompatTextView dateRangeTo;
 
+        readonly char[] dfo;
+
+        DateTime from;
+        DateTime to;
+
         public DateRangeSearchView(Context context)
             : base(context)
         {
@@ -36,10 +42,7 @@ namespace Mark5.Mobile.Droid.Ui.Views.DocumentsSearchViews
                 Adapter = CustomArrayAdapter.CreateWithoutLeftPadding(context, Resource.Array.search_date_range_type, Android.Resource.Layout.SimpleSpinnerItem, Resource.Layout.support_simple_spinner_dropdown_item)
             };
             dateRangeType.SetSelection(0);
-            dateRangeType.ItemSelected += (sender, e) =>
-            {
-                fromToLayout.Visibility = e.Position == 3 ? ViewStates.Visible : ViewStates.Gone;
-            };
+            dateRangeType.ItemSelected += (sender, e) => UpdateFromToFields(e.Position);
             AddView(dateRangeType);
 
             fromToLayout = new LinearLayoutCompat(context)
@@ -53,6 +56,8 @@ namespace Mark5.Mobile.Droid.Ui.Views.DocumentsSearchViews
             fromToLayout.Visibility = ViewStates.Gone;
             AddView(fromToLayout);
 
+            dfo = DateFormat.GetDateFormatOrder(Context);
+
             dateRangeFrom = new AppCompatTextView(context, null, Resource.Attribute.spinnerStyle)
             {
                 LayoutParameters = new LayoutParams(-1, ViewGroup.LayoutParams.WrapContent)
@@ -62,7 +67,7 @@ namespace Mark5.Mobile.Droid.Ui.Views.DocumentsSearchViews
                 }
             };
             dateRangeFrom.SetTextAppearanceCompat(context, Resource.Style.fontPrimary);
-            dateRangeFrom.Text = "22/12/2022";
+            dateRangeFrom.Text = from.ToString($"{dfo[0]}{dfo[0]}/{dfo[1]}{dfo[1]}/{dfo[2]}{dfo[2]}{dfo[2]}{dfo[2]}");
             fromToLayout.AddView(dateRangeFrom);
 
             dateRangeTo = new AppCompatTextView(context, null, Resource.Attribute.spinnerStyle)
@@ -74,16 +79,65 @@ namespace Mark5.Mobile.Droid.Ui.Views.DocumentsSearchViews
                 }
             };
             dateRangeTo.SetTextAppearanceCompat(context, Resource.Style.fontPrimary);
-            dateRangeTo.Text = "22/12/2022";
+            dateRangeTo.Text = to.ToString($"{dfo[0]}{dfo[0]}/{dfo[1]}{dfo[1]}/{dfo[2]}{dfo[2]}{dfo[2]}{dfo[2]}");
             fromToLayout.AddView(dateRangeTo);
+
+            UpdateFromToFields(dateRangeType.SelectedItemPosition);
+        }
+
+        void UpdateFromToFields(int mode)
+        {
+            fromToLayout.Visibility = mode == 3 ? ViewStates.Visible : ViewStates.Gone;
+
+            if (mode == 0)
+            {
+                from = default(DateTime);
+                to = default(DateTime);
+            }
+            if (mode == 1)
+            {
+                var now = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Unspecified).Date;
+                from = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0, DateTimeKind.Unspecified);
+                to = new DateTime(now.Year, now.Month, now.Day, 23, 59, 59, DateTimeKind.Unspecified);
+            }
+            if (mode == 2)
+            {
+                var now = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Unspecified).Date;
+                var yesterday = now.AddDays(-1);
+                from = new DateTime(yesterday.Year, yesterday.Month, yesterday.Day, 0, 0, 0, DateTimeKind.Unspecified);
+                to = new DateTime(yesterday.Year, yesterday.Month, yesterday.Day, 23, 59, 59, DateTimeKind.Unspecified);
+            }
+            if (mode == 3)
+            {
+                var now = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Unspecified).Date;
+                var weekAgo = now.AddDays(-7);
+                from = new DateTime(weekAgo.Year, weekAgo.Month, weekAgo.Day, 0, 0, 0, DateTimeKind.Unspecified);
+                to = new DateTime(now.Year, now.Month, now.Day, 23, 59, 59, DateTimeKind.Unspecified);
+            }
         }
 
         public override void SetFromCriteria(SearchDocumentsCriteria criteria)
         {
+            if (criteria.DateRange == null || !criteria.DateRange.Enabled)
+            {
+                UpdateFromToFields(0);
+            }
+            else
+            {
+                from = criteria.DateRange.Start;
+                to = criteria.DateRange.End;
+                UpdateFromToFields(3);
+            }
         }
 
         public override void UpdateCriteria(SearchDocumentsCriteria criteria)
         {
+            criteria.DateRange = new DateRange
+            {
+                Enabled = dateRangeType.SelectedItemPosition != 0,
+                Start = from,
+                End = to
+            };
         }
     }
 }
