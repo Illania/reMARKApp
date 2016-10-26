582b683993599dafed664adf3fd34c44e1bce483
Ferdinando Papale <fp@nordic-it.com>
2016-10-26 08:52:48 +0200

    Added stub

---
+++ Mark5.Mobile.Droid/Ui/Activities/CategoriesListActivity.cs
@@ -0,0 +1,69 @@
+﻿//
+// Project: 
+// File: CategoriesListActivity.cs
+// Author: Ferdinando Papale fp@nordic-it.com
+//
+// Copyright (c) 2016 Nordic IT
+//
+
+using Android.App;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Common.Utilities;
+using Mark5.Mobile.Droid.Ui.Common;
+
+namespace Mark5.Mobile.Droid
+{
+    [Activity]
+    public class CategoriesListActivity : BaseAppCompatActivity
+    {
+        public const string BusinessEntityPreviewIntentKey = "BusinessEntityPreview_43dc8df1-dc88-4e39-81d6-59ea495c35ff";
+
+        Toolbar toolbar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            CommonConfig.Logger.Info($"Creating {nameof(CategoriesListActivity)}...");
+
+            SetTitle(Resource.String.document);
+            SetContentView(Resource.Layout.base_layout);
+
+            toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+            if (savedInstanceState == null)
+            {
+                var bep = SerializationUtils.Deserialize<BusinessEntityPreview>(Intent.Extras.GetString(BusinessEntityPreviewIntentKey));
+                var ft = SupportFragmentManager.BeginTransaction();
+                var clf = new CategoriesListFragment
+                {
+                    BusinessEntityPreview = bep
+                };
+                ft.Replace(Resource.Id.fragment_container, clf, clf.GenerateTag());
+                ft.Commit();
+
+                CommonConfig.Logger.Info($"Created {nameof(CategoriesListActivity)}");
+            }
+            else
+            {
+                CommonConfig.Logger.Info($"Restored {nameof(CategoriesListActivity)}");
+            }
+        }
+
+        public override bool OnOptionsItemSelected(Android.Views.IMenuItem item)
+        {
+            if (item.ItemId == Android.Resource.Id.Home)
+            {
+                OnBackPressed();
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
---
+++ Mark5.Mobile.Droid/Ui/Fragments/CategoriesListFragment.cs
@@ -0,0 +1,85 @@
+﻿//
+// Project: 
+// File: CategoriesListFragment.cs
+// Author: Ferdinando Papale fp@nordic-it.com
+//
+// Copyright (c) 2016 Nordic IT
+//
+
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Droid.Ui.Common;
+using Mark5.Mobile.Droid.Utilities;
+
+namespace Mark5.Mobile.Droid
+{
+    public class CategoriesListFragment : RetainableStateFragment
+    {
+        public BusinessEntityPreview BusinessEntityPreview
+        {
+            get;
+            set;
+        }
+
+        ScrollView scrollView;
+        LinearLayoutCompat linearLayout;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            CommonConfig.Logger.Info($"Creating {nameof(CategoriesListFragment)} [businessEntity.id={BusinessEntityPreview.Id}, businessEntity.objectType={BusinessEntityPreview.ObjectType}]");
+
+            var rootView = inflater.Inflate(Resource.Layout.linear_layout, container, false);
+
+            scrollView = rootView.FindViewById<ScrollView>(Resource.Id.scroll_view);
+            linearLayout = rootView.FindViewById<LinearLayoutCompat>(Resource.Id.linear_layout);
+
+            return rootView;
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+
+            ((AppCompatActivity)Activity).SupportActionBar.Title = GetString(Resource.String.categories);
+
+            CommonConfig.Logger.Info($"Created {nameof(CategoriesListFragment)} [businessEntity.id={BusinessEntityPreview.Id}, businessEntity.objectType={BusinessEntityPreview.ObjectType}]");
+        }
+
+        #region Retained State
+
+        public override IRetainableState OnRetainInstanceState()
+        {
+            return new CategoriesListFragmentState
+            {
+                BusinessEntityPreview = BusinessEntityPreview,
+            };
+        }
+
+        public override void OnRetainedInstanceStateRestored(IRetainableState restoredState)
+        {
+            var clfs = restoredState as CategoriesListFragmentState;
+            if (clfs != null)
+            {
+                BusinessEntityPreview = clfs.BusinessEntityPreview;
+            }
+        }
+
+        public override string GenerateTag()
+        {
+            return $"{nameof(CategoriesListFragment)} [businessEntity.id={BusinessEntityPreview.Id}, businessEntity.objectType={BusinessEntityPreview.ObjectType}]";
+        }
+
+        class CategoriesListFragmentState : IRetainableState
+        {
+            public BusinessEntityPreview BusinessEntityPreview { get; set; }
+        }
+
+        #endregion
+
+    }
+}
