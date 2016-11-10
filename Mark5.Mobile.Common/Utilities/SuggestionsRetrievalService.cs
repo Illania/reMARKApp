d1c9f70288680a2a43ff602324be3c2c40307705
Ferdinando Papale <fp@nordic-it.com>
2016-11-10 14:43:50 +0100

    Changed internals of suggestions service

--- Mark5.Mobile.Common/Utilities/SuggestionsRetrievalService.cs
+++ Mark5.Mobile.Common/Utilities/SuggestionsRetrievalService.cs
@@ -11,42 +11,58 @@ using System.Threading.Tasks;
 using Mark5.Mobile.Common.Model;
 using Mark5.Mobile.Common.Managers;
 using System.Linq;
+using System.Threading;
 
 namespace Mark5.Mobile.Common.Utilities
 {
     public class SuggestionsRetrievalService
     {
-        public async Task<List<PrintableSuggestion>> GetSuggestionFromRecentAddresses(string phrase)
+        public void GetSuggestions(string phrase, CancellationToken token, Action<List<PrintableSuggestion>, CancellationToken> handler)
         {
-            try
-            {
-                var recentAddresses = await Managers.Managers.DocumentsManager.GetRecentAddressesAsync(); //TODO eventually this should happen only the first time
-                var filtered = recentAddresses.Where(r => r.Address.ContainsCaseInsensitive(phrase) || r.Name.ContainsCaseInsensitive(phrase))
-                                              .Select(ra => new PrintableSuggestion(ra)).ToList();
-                return filtered;
-            }
-            catch (Exception ex)
-            {
-                CommonConfig.Logger.Error("Error while retrieving recent addresses", ex);
-                return new List<PrintableSuggestion>();
-            }
+            GetSuggestionFromRecentAddresses(phrase, token, handler);
+            GetSuggestionFromContacts(phrase, token, handler);
+            GetSuggestionFromPhonebook(phrase, token, handler);
         }
 
-        public async Task<List<PrintableSuggestion>> GetSuggestionFromPhonebook()
+        public void GetSuggestionFromRecentAddresses(string phrase, CancellationToken token, Action<List<PrintableSuggestion>, CancellationToken> handler)
+        {
+            Task.Run(async () =>
+            {
+                var filtered = new List<PrintableSuggestion>();
+                try
+                {
+                    var recentAddresses = await Managers.Managers.DocumentsManager.GetRecentAddressesAsync(); //TODO eventually this should happen only the first time
+                    filtered = recentAddresses.Where(r => r.Address.ContainsCaseInsensitive(phrase) || r.Name.ContainsCaseInsensitive(phrase)) //TODO need to select the right type
+                                                 .Select(ra => new PrintableSuggestion(ra)).ToList();
+                }
+                catch (Exception ex)
+                {
+                    CommonConfig.Logger.Error("Error while retrieving recent addresses", ex);
+                }
+                handler(filtered, token);
+            });
+
+        }
+
+        public void GetSuggestionFromPhonebook(string phrase, CancellationToken token, Action<List<PrintableSuggestion>, CancellationToken> handler)
         {
             var a1 = new PrintableSuggestion("Ferdinando", "fp@nordic-it.com", SuggestionType.Phonebook);
             var a2 = new PrintableSuggestion("Luigi", "ls@nordic-it.com", SuggestionType.Phonebook);
             var a3 = new PrintableSuggestion("Magda", "ma@nordic-it.com", SuggestionType.Phonebook);
-
-            return new List<PrintableSuggestion> { a1, a2, a3 };
+            Task.Run(() =>
+           {
+               handler(new List<PrintableSuggestion> { a1, a2, a3 }, token);
+           });
         }
 
-        public async Task<List<PrintableSuggestion>> GetSuggestionFromContacts()
+        public void GetSuggestionFromContacts(string phrase, CancellationToken token, Action<List<PrintableSuggestion>, CancellationToken> handler)
         {
             var a4 = new PrintableSuggestion("Bartosz", "bgc@nordic-it.com", SuggestionType.Contact);
             var a5 = new PrintableSuggestion("", "fp@nordic-it.com", SuggestionType.Contact);
-
-            return new List<PrintableSuggestion> { a4, a5 };
+            Task.Run(() =>
+           {
+               handler(new List<PrintableSuggestion> { a4, a5 }, token);
+           });
         }
     }
 }
