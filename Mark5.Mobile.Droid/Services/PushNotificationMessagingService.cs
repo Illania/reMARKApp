fd46a202c3d1349afe223b481c0b2e31dfff4b2d
Bartosz Cichecki <bgc@nordic-it.com>
2016-11-23 17:31:40 +0100

    Draft for parsing notification.

--- Mark5.Mobile.Droid/Services/PushNotificationMessagingService.cs
+++ Mark5.Mobile.Droid/Services/PushNotificationMessagingService.cs
@@ -5,10 +5,16 @@
 //
 // Copyright (c) 2016 Nordic IT
 //
+using System;
+using System.Globalization;
 using Android.App;
 using Android.Content;
 using Firebase.Messaging;
 using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Common.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Mark5.Mobile.Droid.Utilities.Services
 {
@@ -20,6 +26,95 @@ namespace Mark5.Mobile.Droid.Utilities.Services
         public override void OnMessageReceived(RemoteMessage message)
         {
             CommonConfig.Logger.Info("Notification received");
+
+            var pn = ConvertToPushNotification(message);
         }
+
+        PushNotification ConvertToPushNotification(RemoteMessage message)
+        {
+            var pn = new PushNotification
+            {
+                Data = SerializationUtils.Deserialize<PushNotificationData>(message.Data["data"]),
+                Notification = SerializationUtils.Deserialize<PushNotificationNotification>(message.Data["notification"])
+            };
+
+            return pn;
+        }
+
+        Common.Model.Notification ConvertToNotification(PushNotification pushnotification)
+        {
+            return new Common.Model.Notification();
+        }
+
+        #region Model
+
+        public class PushNotification
+        {
+
+            public PushNotificationNotification Notification { get; set; }
+
+            public PushNotificationData Data { get; set; }
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        public class PushNotificationNotification
+        {
+
+            [JsonProperty("icon")]
+            public string Icon { get; set; }
+
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("body")]
+            public string Body { get; set; }
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        public class PushNotificationData
+        {
+
+            [JsonProperty("silent")]
+            public int Silent { get; set; }
+
+            public bool IsSilent
+            {
+                get
+                {
+                    return Silent > 0;
+                }
+            }
+
+            [JsonProperty("guid")]
+            public Guid Guid { get; set; }
+
+            [JsonProperty("type")]
+            [JsonConverter(typeof(StringEnumConverter))]
+            public EventType Type { get; set; }
+
+            [JsonProperty("objectId")]
+            public int ObjectId { get; set; }
+
+            [JsonProperty("folderId")]
+            public int FolderId { get; set; }
+
+            [JsonProperty("objectType")]
+            [JsonConverter(typeof(StringEnumConverter))]
+            public ObjectType ObjectType { get; set; }
+
+            [JsonProperty("remindOn")]
+            public string RemindOn { get; set; }
+
+            public long RemindOnTimestamp
+            {
+                get
+                {
+                    return DateTime.ParseExact(RemindOn, "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture).ConvertDateTimeToTimestampMilliseconds();
+                }
+            }
+        }
+
+        #endregion
+
     }
 }
