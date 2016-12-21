3b3777406ccf1b64d1b067d898aee804f2561747
Bartosz Cichecki <bgc@nordic-it.com>
2016-12-21 10:24:31 +0100

    Added empty and wait cells

---
+++ Mark5.Mobile.IOS/Ui/TableViewCells/WaitTableViewCell.designer.cs
@@ -0,0 +1,18 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+
+namespace Mark5.Mobile.IOS.Ui.TableViewCells
+{
+    [Register("WaitTableViewCell")]
+    partial class WaitTableViewCell
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
