f8d2fb3bdcc3b1ac80811cba6367030fa13dded3
Bartosz Cichecki <bgc@nordic-it.com>
2017-02-01 13:16:16 +0100

    basic actions list

---
+++ Mark5.Mobile.IOS/Ui/ViewControllers/ObjectActionsListViewController.cs
@@ -0,0 +1,257 @@
+﻿//
+// Project: Mark5.Mobile.IOS
+// File: ObjectActionsListViewController.cs
+// Author: Bartosz Cichecki <bgc@nordic-it.com>
+//
+// Copyright (c) 2017 Nordic IT
+//
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoreGraphics;
+using Foundation;
+using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Managers;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.IOS.Ui.Common;
+using Mark5.Mobile.IOS.Ui.TableViewCells;
+using UIKit;
+
+namespace Mark5.Mobile.IOS.Ui.ViewControllers
+{
+    
+    public class ObjectActionsListViewController : AbstractViewController
+    {
+
+        readonly IBusinessEntity businessEntity;
+
+        UIBarButtonItem doneItem;
+        UITableView tableView;
+
+        public ObjectActionsListViewController(IBusinessEntity businessEntity)
+        {
+            this.businessEntity = businessEntity;
+        }
+
+        public override void LoadView()
+        {
+            base.LoadView();
+
+            InitializeNavigationBar();
+            InitializeView();
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            InitializeNavigationBarTitle();
+            InitializeHandlers();
+        }
+
+#pragma warning disable RECS0165 // Asynchronous methods should return a Task instead of void
+        public override async void ViewDidAppear(bool animated)
+#pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
+        {
+            base.ViewDidAppear(animated);
+
+            CommonConfig.Logger.Info($"{nameof(ObjectActionsListViewController)} appeared");
+
+            var ds = (DataSource)tableView.Source;
+            if (ds.Empty)
+                await RefreshData();
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+
+            DeinitializeHandlers();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            CommonConfig.Logger.Warning($"{nameof(ObjectActionsListViewController)} received memory warning!");
+
+            var ds = tableView?.DataSource as DataSource;
+            ds?.Reset();
+
+            base.DidReceiveMemoryWarning();
+        }
+
+        void InitializeNavigationBar()
+        {
+            doneItem = new UIBarButtonItem(UIBarButtonSystemItem.Done);
+            doneItem.Enabled = false;
+            NavigationItem.SetRightBarButtonItem(doneItem, false);
+        }
+
+        void InitializeView()
+        {
+            AutomaticallyAdjustsScrollViewInsets = true;
+
+            tableView = new UITableView(CGRect.Empty, UITableViewStyle.Grouped);
+            tableView.ClipsToBounds = false;
+            tableView.Source = new DataSource(this, tableView, Localization.GetString("no_object_actions"));
+            tableView.AllowsSelection = true;
+            tableView.AllowsMultipleSelection = true;
+            tableView.TranslatesAutoresizingMaskIntoConstraints = false;
+            View.AddSubview(tableView);
+            View.AddConstraints(new[]
+                {
+                    NSLayoutConstraint.Create(tableView, NSLayoutAttribute.Top, NSLayoutRelation.Equal, View, NSLayoutAttribute.Top, 1.0f, 0.0f),
+                    NSLayoutConstraint.Create(tableView, NSLayoutAttribute.Left, NSLayoutRelation.Equal, View, NSLayoutAttribute.Left, 1.0f, 0.0f),
+                    NSLayoutConstraint.Create(tableView, NSLayoutAttribute.Right, NSLayoutRelation.Equal, View, NSLayoutAttribute.Right, 1.0f, 0.0f),
+                    NSLayoutConstraint.Create(tableView, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, View, NSLayoutAttribute.Bottom, 1.0f, 0.0f)
+                });
+        }
+
+        void InitializeNavigationBarTitle()
+        {
+            NavigationItem.Title = Localization.GetString("actions");
+        }
+
+        void InitializeHandlers()
+        {
+            if (doneItem != null)
+                doneItem.Clicked += DoneItem_Clicked;
+        }
+
+        void DeinitializeHandlers()
+        {
+            if (doneItem != null)
+                doneItem.Clicked -= DoneItem_Clicked;
+        }
+
+        public void ObjectActionSelected(ObjectAction oa)
+        {
+            // TODO
+        }
+
+        void DoneItem_Clicked(object sender, EventArgs e)
+        {
+            NavigationController.DismissViewController(true, null);
+        }
+
+        async Task RefreshData()
+        {
+            CommonConfig.Logger.Info($"Refreshing list of users");
+
+            try
+            {
+                var objectActions = await Managers.CommonActionsManager.GetObjectActionsAsync(businessEntity);
+                var ds = (DataSource)tableView.Source;
+                ds.SetItems(objectActions);
+            }
+            catch (Exception ex)
+            {
+                CommonConfig.Logger.Error($"Could not refresh list of users", ex);
+
+                await Dialogs.ShowErrorDialogAsync(this, ex);
+            }
+        }
+
+        class DataSource : UITableViewSource, IDisposable
+        {
+
+            public bool Empty
+            {
+                get
+                {
+                    return objectActionsInView.Count < 1;
+                }
+            }
+
+            ObjectActionsListViewController viewController;
+            UITableView tableView;
+            string emptyText;
+
+            bool loading = true;
+            List<ObjectAction> objectActionsInView = new List<ObjectAction>();
+
+            public DataSource(ObjectActionsListViewController viewController, UITableView tableView, string emptyText)
+            {
+                this.viewController = viewController;
+                this.tableView = tableView;
+                this.emptyText = emptyText;
+            }
+
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                if (loading)
+                {
+                    return tableView.DequeueReusableCell(WaitTableViewCell.Key) as WaitTableViewCell ?? WaitTableViewCell.Create();
+                }
+
+                if (objectActionsInView.Count < 1)
+                {
+                    var emptyCell = tableView.DequeueReusableCell(EmptyTableViewCell.Key) as EmptyTableViewCell ?? EmptyTableViewCell.Create();
+                    emptyCell.Initialize(emptyText);
+                    return emptyCell;
+                }
+
+                var oa = objectActionsInView[indexPath.Row];
+
+                var cell = tableView.DequeueReusableCell("subtitle") ?? new UITableViewCell(UITableViewCellStyle.Subtitle, "subtitle");
+                cell.TextLabel.Text = oa.Description;
+                cell.DetailTextLabel.Text = oa.Username;
+
+                return cell;
+            }
+
+            public override nint RowsInSection(UITableView tableview, nint section)
+            {
+                if (loading)
+                    return 1;
+
+                if (objectActionsInView.Count < 1)
+                    return 1;
+
+                return objectActionsInView.Count;
+            }
+
+            public override nint NumberOfSections(UITableView tableView)
+            {
+                return 1;
+            }
+
+            public override nfloat GetHeightForRow(UITableView tableView, NSIndexPath indexPath)
+            {
+                return 44f;
+            }
+
+            public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+            {
+                var su = objectActionsInView[indexPath.Row];
+                viewController.ObjectActionSelected(su);
+            }
+
+            public void SetItems(List<ObjectAction> objectActions)
+            {
+                loading = false;
+
+                objectActionsInView.Clear();
+                objectActionsInView.AddRange(objectActions);
+
+                tableView.ReloadSections(NSIndexSet.FromIndex(0), UITableViewRowAnimation.Automatic);
+            }
+
+            public void Reset()
+            {
+                loading = true;
+
+                objectActionsInView.Clear();
+                tableView.ReloadSections(NSIndexSet.FromIndex(0), UITableViewRowAnimation.Automatic);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                base.Dispose(disposing);
+
+                viewController = null;
+                tableView = null;
+                objectActionsInView = null;
+            }
+        }
+    }
+}
