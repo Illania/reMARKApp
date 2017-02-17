d4c2d1f27d77f555bde7d35382558cc32a6f96c1
Ferdinando Papale <fp@nordic-it.com>
2017-02-17 14:32:32 +0100

    Added notification stub

---
+++ Mark5.Mobile.IOS/Utilities/LocalNotificationsListener.cs
@@ -0,0 +1,27 @@
+﻿//
+// Project: Mark5.Mobile.IOS
+// File: LocalNotificationsListener.cs
+// Author: ferdinandopapale <fp@nordic-it.com>
+//
+// Copyright (c) 2017 Nordic IT
+//
+using System;
+using Mark5.Mobile.Common.Managers;
+using Mark5.Mobile.Common.Model;
+using UserNotifications
+
+namespace Mark5.Mobile.IOS.Utilities
+{
+    public static class LocalNotificationsListener
+    {
+        static public void Initialize()
+        {
+            Managers.OutgoingDocumentsManager.DocumentSendingFailed += OutgoingDocumentsManager_DocumentSendingFailed; ;
+        }
+
+        static void OutgoingDocumentsManager_DocumentSendingFailed(object sender, OutgoingDocumentContainer e)
+        {
+            var notification = new UNMutableNotificationContent();
+        }
+    }
+}
