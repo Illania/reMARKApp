7a5624f897271681904e6d998c954bb7a9b36d49
Ferdinando Papale <fp@nordic-it.com>
2017-02-17 16:08:04 +0100

    Added notification for failed send document

--- Mark5.Mobile.IOS/Utilities/LocalNotificationsListener.cs
+++ Mark5.Mobile.IOS/Utilities/LocalNotificationsListener.cs
@@ -5,23 +5,43 @@
 //
 // Copyright (c) 2017 Nordic IT
 //
-using System;
+using Mark5.Mobile.Common;
 using Mark5.Mobile.Common.Managers;
 using Mark5.Mobile.Common.Model;
-using UserNotifications
+using Mark5.Mobile.IOS.Ui.Common;
+using UserNotifications;
 
 namespace Mark5.Mobile.IOS.Utilities
 {
     public static class LocalNotificationsListener
     {
+        public const string FailedSendingIdentifier = "FailedSendingIdentifier";
+
         static public void Initialize()
         {
-            Managers.OutgoingDocumentsManager.DocumentSendingFailed += OutgoingDocumentsManager_DocumentSendingFailed; ;
+            Managers.OutgoingDocumentsManager.DocumentSendingFailed += OutgoingDocumentsManager_DocumentSendingFailed;
         }
 
         static void OutgoingDocumentsManager_DocumentSendingFailed(object sender, OutgoingDocumentContainer e)
         {
-            var notification = new UNMutableNotificationContent();
+            var notificatioContent = new UNMutableNotificationContent();
+
+            var titleString = Localization.GetString("failed_send_document_notification_title");
+            var contentString = Localization.GetString("failed_send_document_notification_content");
+
+            notificatioContent.Title = titleString;
+            notificatioContent.Body = contentString;
+
+            var request = UNNotificationRequest.FromIdentifier(FailedSendingIdentifier, notificatioContent, null);
+
+            UNUserNotificationCenter.Current.AddNotificationRequest(request, err =>
+            {
+                if (err != null)
+                {
+                    CommonConfig.Logger.Error($"Error while sending notification for failed send document: {err}");
+                }
+            });
+
         }
     }
 }
