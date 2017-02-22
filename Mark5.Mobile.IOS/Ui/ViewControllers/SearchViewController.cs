744d457d950ca82a4d0f8a8a154e385a0006ec5c
Bartosz Cichecki <bgc@nordic-it.com>
2017-02-22 16:01:45 +0100

    Added fake search page.

--- Mark5.Mobile.IOS/Ui/ViewControllers/SearchViewController.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/SearchViewController.cs
@@ -5,12 +5,55 @@
 //
 // Copyright (c) 2016 Nordic IT
 //
+using Mark5.Mobile.Common.Model;
 using Mark5.Mobile.IOS.Ui.Common;
+using UIKit;
 
 namespace Mark5.Mobile.IOS.Ui.ViewControllers
 {
 
     public class SearchViewController : AbstractViewController
     {
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            View.BackgroundColor = UIColor.White;
+
+            var docs = new UIButton(new CoreGraphics.CGRect(100, 100, 100, 50));
+            docs.SetTitle("Docs", UIControlState.Normal);
+            docs.SetTitleColor(UIColor.Black, UIControlState.Normal);
+            View.AddSubview(docs);
+
+            var con = new UIButton(new CoreGraphics.CGRect(100, 200, 100, 50));
+            con.SetTitle("Contacts", UIControlState.Normal);
+            con.SetTitleColor(UIColor.Black, UIControlState.Normal);
+            View.AddSubview(con);
+
+            var sho = new UIButton(new CoreGraphics.CGRect(100, 300, 100, 50));
+            sho.SetTitle("Shortcodes", UIControlState.Normal);
+            sho.SetTitleColor(UIColor.Black, UIControlState.Normal);
+            View.AddSubview(sho);
+
+
+            docs.TouchUpInside += (sender, e) =>
+            {
+                var vc = new DocumentsSearchResultsViewController { Criteria = new SearchDocumentsCriteria() };
+                NavigationController.PushViewController(vc, true);
+            };
+
+            con.TouchUpInside += (sender, e) =>
+            {
+                var vc = new ContactsSearchResultsViewController { Criteria = new SearchContactsCriteria() };
+                NavigationController.PushViewController(vc, true);
+            };
+
+            sho.TouchUpInside += (sender, e) =>
+            {
+                var vc = new ShortcodesSearchResultsViewController { Criteria = new SearchShortcodesCriteria() };
+                NavigationController.PushViewController(vc, true);
+            };
+        }
     }
 }
