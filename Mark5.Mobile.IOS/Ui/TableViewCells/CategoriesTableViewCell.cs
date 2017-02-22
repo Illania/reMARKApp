13b752df8376e936396544f5e27931a3a3010d03
Ferdinando Papale <fp@nordic-it.com>
2017-02-22 13:56:36 +0100

    Added cell

---
+++ Mark5.Mobile.IOS/Ui/TableViewCells/CategoriesTableViewCell.cs
@@ -0,0 +1,30 @@
+﻿//
+// Project: Mark5.Mobile.IOS
+// File: CategoriesListViewCell.cs
+// Author: ferdinandopapale <fp@nordic-it.com>
+//
+// Copyright (c) 2017 Nordic IT
+//
+using System;
+
+using Foundation;
+using UIKit;
+
+namespace Mark5.Mobile.IOS.Ui.TableViewCells
+{
+    public partial class CategoriesListViewCell : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString("CategoriesListViewCell");
+        public static readonly UINib Nib;
+
+        static CategoriesListViewCell()
+        {
+            Nib = UINib.FromName("CategoriesListViewCell", NSBundle.MainBundle);
+        }
+
+        protected CategoriesListViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
