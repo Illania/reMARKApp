d7889970beddab8eea0eee8fb02ff0ab79df9d10
Bartosz Cichecki <bgc@nordic-it.com>
2017-03-07 09:16:05 +0100

    minor ui/text corrections #6

--- Mark5.Mobile.Common/Utilities/StringExtensions.cs
+++ Mark5.Mobile.Common/Utilities/StringExtensions.cs
@@ -18,9 +18,7 @@ namespace Mark5.Mobile.Common.Utilities
         public static string SafeSubstring(this string str, int startIndex)
         {
             if (str.Length <= startIndex)
-            {
                 return str;
-            }
 
             return str.Substring(startIndex);
         }
@@ -28,46 +26,32 @@ namespace Mark5.Mobile.Common.Utilities
         public static string SafeSubstring(this string str, int startIndex, int length)
         {
             if (str.Length <= startIndex + length)
-            {
                 return str;
-            }
 
             return str.Substring(startIndex, length);
         }
 
-        public static bool ContainsCaseInsensitive(this string source, string toCheck)
-        {
-            if (toCheck == null)
-            {
-                throw new ArgumentException("The toCheck string cannot be null");
-            }
-
-            return source.IndexOf(toCheck, StringComparison.CurrentCultureIgnoreCase) >= 0;
-        }
-
-        public static string SafeSubstringBefore(this string str, string value, StringComparison comparisonType)
+        public static string SafeSubstringBefore(this string str, string value, StringComparison comparisonType = StringComparison.CurrentCulture)
         {
             var index = str.IndexOf(value, comparisonType);
             return index > 0 ? str.SafeSubstring(0, index) : str;
         }
 
-        public static string SafeSubstringBeforeLast(this string str, char value)
+        public static string SafeSubstringBeforeLast(this string str, string value, StringComparison comparisonType = StringComparison.CurrentCulture)
         {
-            var index = str.LastIndexOf(value);
+            var index = str.LastIndexOf(value, comparisonType);
             return index > 0 ? str.SafeSubstring(0, index) : str;
         }
 
-        public static string SafeSubstringAfterLast(this string str, char value)
+        public static string SafeSubstringAfterLast(this string str, string value, StringComparison comparisonType = StringComparison.CurrentCulture)
         {
-            var index = str.LastIndexOf(value);
+            var index = str.LastIndexOf(value, comparisonType);
             return index > 0 ? str.SafeSubstring(index + 1) : str;
         }
 
-        public static string SafeSubstringAfterLast(this string str, string value, StringComparison comparisonType)
+        public static bool ContainsCaseInsensitive(this string str, string value)
         {
-            var index = str.LastIndexOf(value, comparisonType);
-            return index > 0 ? str.SafeSubstring(index + value.Length) : str;
+            return str.IndexOf(value, StringComparison.CurrentCultureIgnoreCase) >= 0;
         }
-
     }
 }
