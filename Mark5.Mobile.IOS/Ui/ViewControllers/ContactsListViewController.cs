bd5f3c60f47540809fd4e4a24d19556ec8b50922
Ferdinando Papale <fp@nordic-it.com>
2017-03-16 15:32:34 +0100

    Added show categories to swipe actions for contacts

--- Mark5.Mobile.IOS/Ui/ViewControllers/ContactsListViewController.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/ContactsListViewController.cs
@@ -564,6 +564,12 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers
             PresentViewController(new NavigationController(vc, UIModalPresentationStyle.PageSheet), true, null);
         }
 
+        void ShowCategories(ContactPreview selectedContact)
+        {
+            var vc = new CategoriesListViewController { BusinessEntityPreview = selectedContact };
+            PresentViewController(new NavigationController(vc, UIModalPresentationStyle.PageSheet), true, null);
+        }
+
         void MoveToFolder(ContactPreview selectedContact) => MoveToFolder(new List<ContactPreview> { selectedContact });
 
         void MoveToFolder(List<ContactPreview> selectedContacts)
@@ -575,6 +581,13 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers
         void DoShowMoreActionSheet(NSIndexPath indexPath, ContactPreview selectedContact)
         {
             var eas = UIAlertController.Create(null, null, UIAlertControllerStyle.ActionSheet);
+
+
+            eas.AddAction(UIAlertAction.Create(Localization.GetString("categories"), UIAlertActionStyle.Default, a =>
+            {
+                ShowCategories(selectedContact);
+                EndEditing();
+            }));
 
             eas.AddAction(UIAlertAction.Create(Localization.GetString("copy_to_folder"), UIAlertActionStyle.Default, a =>
             {
