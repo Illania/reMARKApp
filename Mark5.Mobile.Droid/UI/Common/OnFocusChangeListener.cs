98ffa4ea1dedd601d87c91968d197e868167c068
Ferdinando Papale <fp@nordic-it.com>
2017-03-27 14:00:57 +0200

    Added onFocusChangeListener plus stub for priority search

---
+++ Mark5.Mobile.Droid/UI/Common/OnFocusChangeListener.cs
@@ -0,0 +1,30 @@
+﻿//
+// Project: Mark5.Mobile.Droid
+// File: OnFocusChangeListener.cs
+// Author: ferdinandopapale <fp@nordic-it.com>
+//
+// Copyright (c) 2017 Nordic IT
+//
+using System;
+using Android.Views;
+
+namespace Mark5.Mobile.Droid.Ui.Common
+{
+    public class OnFocusChangeListener : Java.Lang.Object, View.IOnFocusChangeListener
+    {
+        readonly Action<View, bool> action;
+
+        public OnFocusChangeListener(Action<View, bool> action)
+        {
+            this.action = action;
+        }
+
+        public void OnFocusChange(View v, bool hasFocus)
+        {
+            if (action != null)
+            {
+                action(v, hasFocus);
+            }
+        }
+    }
+}
---
+++ Mark5.Mobile.Droid/UI/Views/SearchViews/DocumentPrioritySearchView.cs
@@ -0,0 +1,35 @@
+﻿//
+// Project: Mark5.Mobile.Droid
+// File: DocumentPrioritySearchView.cs
+// Author: ferdinandopapale <fp@nordic-it.com>
+//
+// Copyright (c) 2017 Nordic IT
+//
+using System.Collections.Generic;
+using Mark5.Mobile.Common.Model;
+
+namespace Mark5.Mobile.Droid.Ui.Views.SearchViews
+{
+    public class DocumentPrioritySearchView : AbstractDropdownSearchView<SearchDocumentsCriteria, Priority>
+    {
+        public DocumentPrioritySearchView(Android.Content.Context context)
+            : base(context, Resource.String.search_document_priorities, Resource.String.search_document_priorities_none_selected)
+        {
+            Values = new List<Priority> { Priority.Urgent, Priority.Normal, Priority.Low };
+        }
+
+        protected override void ClickAction()
+        {
+        }
+
+        public override void FromCriteria(SearchDocumentsCriteria criteria)
+        {
+            //TODO
+        }
+
+        public override void ToCriteria(SearchDocumentsCriteria criteria)
+        {
+            //TODO
+        }
+    }
+}
