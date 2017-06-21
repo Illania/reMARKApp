6a30faded410b5e932e5c044a91fb9a5e7122bc2
Ferdinando Papale <fp@nordic-it.com>
2017-06-21 13:48:26 +0200

    Correction for iOS

--- Mark5.Mobile.IOS/Ui/ViewControllers/ComposeDocumentView/ComposeDocumentViewController.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/ComposeDocumentView/ComposeDocumentViewController.cs
@@ -610,7 +610,7 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
                         ModalPresentationStyle = UIModalPresentationStyle.PageSheet
                     };
                     if (picker.PopoverPresentationController != null)
-                        picker.PopoverPresentationController.Delegate = new PopoverPresentationControllerDelegate((UIBarButtonItem) sender);
+                        picker.PopoverPresentationController.Delegate = new PopoverPresentationControllerDelegate((UIBarButtonItem)sender);
                     PresentViewController(picker, true, null);
                 }));
             sourceChooser.AddAction(UIAlertAction.Create(Localization.GetString("existing_photo"),
@@ -629,7 +629,7 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
                         ModalPresentationStyle = UIModalPresentationStyle.PageSheet
                     };
                     if (picker.PopoverPresentationController != null)
-                        picker.PopoverPresentationController.Delegate = new PopoverPresentationControllerDelegate((UIBarButtonItem) sender);
+                        picker.PopoverPresentationController.Delegate = new PopoverPresentationControllerDelegate((UIBarButtonItem)sender);
                     PresentViewController(picker, true, null);
                 }));
             sourceChooser.AddAction(UIAlertAction.Create(Localization.GetString("browse_files"),
@@ -648,13 +648,13 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
                         Delegate = new DocumentMenuDelegate(this, HandleAttachmentUrl)
                     };
                     if (picker.PopoverPresentationController != null)
-                        picker.PopoverPresentationController.Delegate = new PopoverPresentationControllerDelegate((UIBarButtonItem) sender);
+                        picker.PopoverPresentationController.Delegate = new PopoverPresentationControllerDelegate((UIBarButtonItem)sender);
                     PresentViewController(picker, true, null);
                 }));
             sourceChooser.AddAction(UIAlertAction.Create(Localization.GetString("cancel"), UIAlertActionStyle.Cancel, null));
 
             if (sourceChooser.PopoverPresentationController != null)
-                sourceChooser.PopoverPresentationController.Delegate = new PopoverPresentationControllerDelegate((UIBarButtonItem) sender);
+                sourceChooser.PopoverPresentationController.Delegate = new PopoverPresentationControllerDelegate((UIBarButtonItem)sender);
 
             PresentViewController(sourceChooser, true, null);
         }
@@ -717,7 +717,7 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
 
             try
             {
-                var sizeInBytes = (long) jpegData.Length;
+                var sizeInBytes = (long)jpegData.Length;
                 stream = jpegData.AsStream();
 
                 if (sizeInBytes > ServerConfig.SystemSettings.DocumentsModuleInfo.MaximumAttachmentSizeBytes)
@@ -858,6 +858,8 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
                     await DoOpenPhonebook(sender as RecipientsView);
                     break;
             }
+
+            sendButtonItem.Enabled = IsFormValid();
         }
 
         void RecipientView_SearchRequested(object sender, string initialSearchString)
@@ -882,7 +884,7 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
                 View.SendSubviewToBack(suggestionsListView);
             }
 
-            var recipientView = (RecipientsView) sender;
+            var recipientView = (RecipientsView)sender;
             suggestionsListView.Initialize(recipientView, initialSearchString);
 
             View.BringSubviewToFront(suggestionsListView);
@@ -1281,12 +1283,12 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
                 try
                 {
                     NSData jpegImage;
-                    using (var image = (UIImage) info[UIImagePickerController.OriginalImage])
+                    using (var image = (UIImage)info[UIImagePickerController.OriginalImage])
                     {
                         jpegImage = image.AsJPEG();
                     }
 
-                    var referenceUrl = (NSUrl) info[UIImagePickerController.ReferenceUrl];
+                    var referenceUrl = (NSUrl)info[UIImagePickerController.ReferenceUrl];
 
                     string filename;
                     if (referenceUrl != null)
@@ -1296,7 +1298,7 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
                                 referenceUrl
                             },
                             null);
-                        var asset = (PHAsset) results.firstObject;
+                        var asset = (PHAsset)results.firstObject;
 
                         var assetResources = PHAssetResource.GetAssetResources(asset);
                         filename = assetResources[0].OriginalFilename;
