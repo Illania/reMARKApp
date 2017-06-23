4a2286c5b3487c23eabb50c9bd4749729975069e
Bartosz Cichecki <bgc@nordic-it.com>
2017-06-23 09:58:14 +0200

    M5APP-305

--- Mark5.ServiceReference/AppServiceProxyFactory.cs
+++ Mark5.ServiceReference/AppServiceProxyFactory.cs
@@ -8,11 +8,7 @@ namespace Mark5.ServiceReference
     {
         public static IAppServiceProxy Create(bool ssl, string hostname, int port, Func<HttpMessageHandler> httpClientHandler)
         {
-#if DEBUG
             return new HttpAppServiceProxy(ssl, hostname, port, httpClientHandler);
-#else
-            return new WcfAppServiceProxy(ssl, hostname, port);
-#endif
         }
     }
 }
