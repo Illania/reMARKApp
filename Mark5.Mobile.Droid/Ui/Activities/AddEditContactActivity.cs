2cbbf0a3ca8c8eb4e1e377e0f9822a79fc13c197
Ferdinando Papale <fp@nordic-it.com>
2017-06-27 11:02:47 +0200

    Android stub

---
+++ Mark5.Mobile.Droid/Ui/Activities/AddEditContactActivity.cs
@@ -0,0 +1,62 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Common.Utilities;
+using Mark5.Mobile.Droid.Ui.Fragments;
+
+namespace Mark5.Mobile.Droid.Ui.Activities
+{
+    [Activity(ScreenOrientation = ScreenOrientation.Portrait)]
+    public class AddEditContactActivity : AppCompatActivity
+    {
+        const string ContactIntentKey = "Contact_16fd7751-f195-4a43-87fa-097115921e6d";
+
+        Toolbar toolbar;
+
+        public static Intent CreateIntent(Contact contact)
+        {
+            var intent = new Intent();
+            intent.PutExtra(ContactIntentKey, SerializationUtils.Serialize(contact));
+            return intent;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            CommonConfig.Logger.Info($"Creating {nameof(AddEditContactActivity)}...");
+
+            OverridePendingTransition(Resource.Animation.slide_up, Resource.Animation.no_change);
+
+            SetContentView(Resource.Layout.base_layout);
+
+            toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+            if (savedInstanceState == null)
+            {
+                var aecf = new AddEditContactFragment();
+
+                if (Intent.HasExtra(ContactIntentKey))
+                    aecf.Contact = SerializationUtils.Deserialize<Contact>(Intent.Extras.GetString(ContactIntentKey));
+
+                var ft = SupportFragmentManager.BeginTransaction();
+                ft.Replace(Resource.Id.fragment_container, aecf, aecf.GenerateTag());
+                ft.Commit();
+
+                CommonConfig.Logger.Info($"Created {nameof(AddEditContactActivity)}");
+            }
+            else
+            {
+                CommonConfig.Logger.Info($"Restored {nameof(AddEditContactActivity)}");
+            }
+        }
+    }
+}
---
+++ Mark5.Mobile.Droid/Ui/Fragments/AddEditContactFragment.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Droid.Ui.Common;
+
+namespace Mark5.Mobile.Droid.Ui.Fragments
+{
+    public class AddEditContactFragment : RetainableStateFragment
+    {
+        public Contact Contact { get; set; }
+
+        public override string GenerateTag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+    }
+}
