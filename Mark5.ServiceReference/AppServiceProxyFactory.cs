aed5ca262562ff5acbf8473b5fcf35f11efcbdc5
Bartosz Cichecki <bgc@nordic-it.com>
2017-06-30 10:27:50 +0200

    Revert "M5APP-305"
    This reverts commit 4a2286c5b3487c23eabb50c9bd4749729975069e.

--- Mark5.ServiceReference/AppServiceProxyFactory.cs
+++ Mark5.ServiceReference/AppServiceProxyFactory.cs
@@ -8,7 +8,11 @@ namespace Mark5.ServiceReference
     {
         public static IAppServiceProxy Create(bool ssl, string hostname, int port, Func<HttpMessageHandler> httpClientHandler)
         {
+#if DEBUG
             return new HttpAppServiceProxy(ssl, hostname, port, httpClientHandler);
+#else
+            return new WcfAppServiceProxy(ssl, hostname, port);
+#endif
         }
     }
 }
