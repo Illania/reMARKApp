4a4d50c84b8a01575d0d9d863b23a40d877cd207
Ferdinando Papale <fp@nordic-it.com>
2017-07-03 12:25:26 +0200

    Added abstract class for user selection

---
+++ Mark5.Mobile.Droid/Ui/Fragments/ResponsibleSelectionFragment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Mark5.Mobile.Droid.Ui.Fragments
+{
+    public class ResponsibleSelectionFragment : AbstractUserSelectionFragment
+    {
+        public ResponsibleSelectionFragment()
+            : base(Resource.String.confirm, true)
+        {
+        }
+
+        public override string GenerateTag()
+        {
+            return $"{nameof(ResponsibleSelectionFragment)}";
+        }
+
+        protected override void ActionButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetInfo()
+        {
+            return $"[preselectedEntitites.Count ={PreselectedUserIds?.Count}]";
+        }
+    }
+}
