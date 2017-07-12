c25be0524dbd291c41083f4057d7b0803006efcf
Ferdinando Papale <fp@nordic-it.com>
2017-07-12 16:21:15 +0200

    Added stub

---
+++ Mark5.Mobile.IOS/Ui/ViewControllers/AddEditContactView/AddEditContacViewController.cs
@@ -0,0 +1,50 @@
+﻿using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.IOS.Ui.Common;
+using UIKit;
+
+namespace Mark5.Mobile.IOS.Ui.ViewControllers.AddEditContactView
+{
+    public class AddEditContacViewController : AbstractViewController
+    {
+        public Contact Contact { get; set; }
+        public ContactPreview ContactPreview { get; set; }
+        public Folder Folder { get; set; }
+        public int? FolderId { get; set; }
+        public int? ContactId { get; set; }
+        public ContactType ContactType { get; set; }
+        public ContactCreationModeFlag CreationModeFlag { get; set; }
+        public ContactPreview ParentContactPreview { get; set; }
+
+        UIBarButtonItem editButton;
+
+        #region UIViewControllerOverrides
+
+        public override void LoadView()
+        {
+            base.LoadView();
+
+            InitNavigationBar();
+            InitView();
+        }
+
+        #endregion
+
+        #region Init methods
+
+        void InitNavigationBar()
+        {
+            editButton = new UIBarButtonItem();
+            //editButton.Image = UIImage.FromBundle(Path.Combine("icons", "compose.png"));
+            editButton.Enabled = false;
+            NavigationItem.SetRightBarButtonItem(editButton, false);
+        }
+
+        void InitView()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Mark5.Mobile.IOS/Ui/ViewControllers/AddEditContactView/Subviews/AddEditContactSubView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.IOS.Ui.Common;
+using UIKit;
+
+namespace Mark5.Mobile.IOS.Ui.ViewControllers.AddEditContactView.Subviews
+{
+    public abstract class AddEditContactSubView : UIStackView
+    {
+        public event EventHandler Edited = delegate { };
+
+        public Contact Contact { get; set; }
+        public ContactPreview ContactPreview { get; set; }
+        public ContactPreview ParentContactPreview { get; set; }
+        public ContactCreationModeFlag CreationMode { get; set; }
+
+        protected UIView ContainerView;
+
+        protected float HorizontalMargin = 15f;
+        protected float VerticalMargin = 12f;
+        protected float InnerMargin = 5f;
+
+        protected AddEditContactSubView()
+        {
+            BackgroundColor = UIColor.White;
+            Opaque = false;
+            Axis = UILayoutConstraintAxis.Vertical;
+            Alignment = UIStackViewAlignment.Fill;
+            Distribution = UIStackViewDistribution.Fill;
+            Spacing = 0f;
+            TranslatesAutoresizingMaskIntoConstraints = false;
+
+            ContainerView = new UIView();
+            AddArrangedSubview(ContainerView);
+
+            AddArrangedSubview(new SeparatorSubView());
+        }
+
+
+        //This needs to be used by all subclasses that can contain invalid content
+        protected void OnContentChanged()
+        {
+            Edited(this, EventArgs.Empty);
+        }
+
+        abstract public void RefreshView();
+        abstract public bool ContainsValidContent();
+    }
+}
