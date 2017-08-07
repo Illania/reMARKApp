9d172128ffd877be1cf3edf3736e799a3172983d
Ferdinando Papale <fp@nordic-it.com>
2017-08-07 11:14:47 +0200

    Corrections

--- Mark5.Mobile.IOS/Ui/TableViewCells/AddEditContactTableViewCell/DisclosureIndicatorTableViewCell.cs
+++ Mark5.Mobile.IOS/Ui/TableViewCells/AddEditContactTableViewCell/DisclosureIndicatorTableViewCell.cs
@@ -12,6 +12,9 @@ namespace Mark5.Mobile.IOS.Ui.TableViewCells.AddEditContactTableViewCell
         protected UILabel TitleLabel { get; set; }
         protected UILabel ContentLabel { get; set; }
 
+        NSLayoutConstraint leftContentConstraint;
+        NSLayoutConstraint leftTitleConstraint;
+
         public DisclosureIndicatorTableViewCell()
             : base(UITableViewCellStyle.Default, Key)
         {
@@ -28,12 +31,12 @@ namespace Mark5.Mobile.IOS.Ui.TableViewCells.AddEditContactTableViewCell
             TitleLabel.SetContentHuggingPriority((float)UILayoutPriority.Required, UILayoutConstraintAxis.Horizontal);
             TitleLabel.SetContentCompressionResistancePriority((float)UILayoutPriority.Required, UILayoutConstraintAxis.Horizontal);
             ContentView.AddSubview(TitleLabel);
-
+            leftTitleConstraint = NSLayoutConstraint.Create(TitleLabel, NSLayoutAttribute.Left, NSLayoutRelation.Equal, ContentView, NSLayoutAttribute.LeftMargin, 1f, HorizontalMargin);
             ContentView.AddConstraints(new[]
             {
                 NSLayoutConstraint.Create(TitleLabel, NSLayoutAttribute.Top, NSLayoutRelation.Equal, ContentView, NSLayoutAttribute.TopMargin, 1f, VerticalMargin),
-                NSLayoutConstraint.Create(TitleLabel, NSLayoutAttribute.Left, NSLayoutRelation.Equal, ContentView, NSLayoutAttribute.LeftMargin, 1f, HorizontalMargin),
                 NSLayoutConstraint.Create(TitleLabel, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, ContentView, NSLayoutAttribute.BottomMargin, 1f, -VerticalMargin),
+                leftTitleConstraint,
             });
 
             ContentLabel = new UILabel
@@ -43,20 +46,30 @@ namespace Mark5.Mobile.IOS.Ui.TableViewCells.AddEditContactTableViewCell
                 Lines = 0,
             };
             ContentView.AddSubview(ContentLabel);
+            leftContentConstraint = NSLayoutConstraint.Create(ContentLabel, NSLayoutAttribute.Left, NSLayoutRelation.Equal, TitleLabel, NSLayoutAttribute.Right, 1f, InnerHorizontalMargin);
             ContentView.AddConstraints(new[]
             {
                 NSLayoutConstraint.Create(ContentLabel, NSLayoutAttribute.Height, NSLayoutRelation.GreaterThanOrEqual, null, NSLayoutAttribute.Height, 1f, 20f),
                 NSLayoutConstraint.Create(ContentLabel, NSLayoutAttribute.Top, NSLayoutRelation.Equal, ContentView, NSLayoutAttribute.TopMargin, 1f, VerticalMargin),
-                NSLayoutConstraint.Create(ContentLabel, NSLayoutAttribute.Left, NSLayoutRelation.Equal, TitleLabel, NSLayoutAttribute.Right, 1f, InnerHorizontalMargin),
                 NSLayoutConstraint.Create(ContentLabel, NSLayoutAttribute.Right, NSLayoutRelation.Equal, ContentView, NSLayoutAttribute.RightMargin, 1f, -HorizontalMargin),
                 NSLayoutConstraint.Create(ContentLabel, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, ContentView, NSLayoutAttribute.BottomMargin, 1f, -VerticalMargin),
+                leftContentConstraint,
             });
-            //TODO there are still the margins when the label is not there
         }
 
         public void SetTitle(string title)
         {
-            TitleLabel.Text = title;
+            if (string.IsNullOrEmpty(title))
+            {
+                leftTitleConstraint.Constant = 0;
+                leftContentConstraint.Constant = HorizontalMargin;
+            }
+            else
+            {
+                leftTitleConstraint.Constant = HorizontalMargin;
+                leftContentConstraint.Constant = InnerHorizontalMargin;
+                TitleLabel.Text = title;
+            }
         }
 
         public void SetContent(string content)
@@ -66,8 +79,8 @@ namespace Mark5.Mobile.IOS.Ui.TableViewCells.AddEditContactTableViewCell
 
         public void Reset()
         {
-            TitleLabel.Text = string.Empty;
-            ContentLabel.Text = string.Empty;
+            SetTitle(string.Empty);
+            SetContent(string.Empty);
         }
     }
 }
