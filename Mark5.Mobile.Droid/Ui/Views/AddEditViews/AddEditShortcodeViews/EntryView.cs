2522d7c275a5cad3c578737312a009260093a89d
Ferdinando Papale <fp@nordic-it.com>
2017-09-06 15:42:19 +0200

    Added subviews

---
+++ Mark5.Mobile.Droid/Ui/Views/AddEditViews/AddEditShortcodeViews/EntryView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.Content;
+using Mark5.Mobile.Common.Model;
+
+namespace Mark5.Mobile.Droid.Ui.Views.AddEditShortcodeViews
+{
+    public class EntryView : AbstractMultipleRowsView<DocumentAddress>
+    {
+        public EntryView(Context context, int titleResourceId)
+            : base(context, titleResourceId)
+        {
+        }
+
+        public override void RefreshView()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AddButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Row GetNewRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Row_DeleteClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mark5.Mobile.Droid/Ui/Views/AddEditViews/AddEditShortcodeViews/NameView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.Content;
+using Android.Text;
+
+namespace Mark5.Mobile.Droid.Ui.Views.AddEditShortcodeViews
+{
+    public class NameView : AbstractSimpleFieldView
+    {
+        public NameView(Context context)
+            : base(context, Resource.String.edit_shortcode_name, false, true, -1,
+                   InputTypes.ClassText | InputTypes.TextFlagCapSentences | InputTypes.TextFlagNoSuggestions)
+        {
+        }
+
+        public override void RefreshView()
+        {
+            Content = ShortcodePreview.Name;
+        }
+
+        protected override void ContentChanged(object sender, TextChangedEventArgs e)
+        {
+            ShortcodePreview.Name = Content;
+        }
+    }
+}
