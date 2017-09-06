4e53d80260168a7a4d218eef9a2640d2525d0f91
Ferdinando Papale <fp@nordic-it.com>
2017-09-06 16:18:00 +0200

    Corrections

--- Mark5.Mobile.Droid/Ui/Views/AddEditViews/AddEditShortcodeViews/EntryView.cs
+++ Mark5.Mobile.Droid/Ui/Views/AddEditViews/AddEditShortcodeViews/EntryView.cs
@@ -1,19 +1,50 @@
 ﻿using System;
+using System.Linq;
 using Android.Content;
+using Android.Support.V7.Widget;
+using Android.Text;
+using Android.Views;
 using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Common.Utilities;
+using Mark5.Mobile.Droid.Ui.Common;
 
 namespace Mark5.Mobile.Droid.Ui.Views.AddEditShortcodeViews
 {
     public class EntryView : AbstractMultipleRowsView<DocumentAddress>
     {
-        public EntryView(Context context, int titleResourceId)
-            : base(context, titleResourceId)
+        DocumentAddressType addressType;
+
+        public EntryView(Context context, DocumentAddressType type)
+            : base(context, ResourceIdForType(type))
         {
+            addressType = type;
+        }
+
+        static int ResourceIdForType(DocumentAddressType type)
+        {
+            switch (type)
+            {
+                case DocumentAddressType.To:
+                    return Resource.String.to;
+                case DocumentAddressType.Cc:
+                    return Resource.String.cc;
+                case DocumentAddressType.Bcc:
+                    return Resource.String.bcc;
+                default:
+                    throw new ArgumentException("Invalid type");
+            }
         }
 
         public override void RefreshView()
         {
-            throw new NotImplementedException();
+            Clear();
+
+            var addresses = Shortcode.Addresses.Where(
+                a => a.Type == CommunicationAddressType.Email && a.AddressType == addressType);
+            foreach (var address in addresses)
+            {
+                AddRow(address);
+            }
         }
 
         protected override void AddButton_Click(object sender, EventArgs e)
@@ -23,12 +54,133 @@ namespace Mark5.Mobile.Droid.Ui.Views.AddEditShortcodeViews
 
         protected override Row GetNewRow()
         {
-            throw new NotImplementedException();
+            return new EntryRow(Context, this);
         }
 
         protected override void Row_DeleteClicked(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            var row = sender as EntryRow;
+            var ca = row.GetContent();
+            Shortcode.Addresses.Remove(ca);
+            RemoveRow(row);
+        }
+
+        async void CreateDialog(EntryRow row = null)
+        {
+            DocumentAddress da = null;
+
+            var container = new LinearLayoutCompat(Context)
+            {
+                Orientation = Vertical,
+                LayoutParameters = new LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent)
+            };
+
+            var emailEditText = new AppCompatEditText(Context)
+            {
+                LayoutParameters = new LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent)
+            };
+            emailEditText.SetHint(Resource.String.edit_shortcode_address);
+            emailEditText.SetTextAppearanceCompat(Context, Resource.Style.fontPrimary);
+            emailEditText.InputType = InputTypes.TextVariationEmailAddress | InputTypes.ClassText;
+            container.AddView(emailEditText);
+
+            var nameEditText = new AppCompatEditText(Context)
+            {
+                LayoutParameters = new LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent)
+            };
+            nameEditText.SetHint(Resource.String.edit_shortcode_name);
+            nameEditText.SetTextAppearanceCompat(Context, Resource.Style.fontPrimary);
+            nameEditText.InputType = InputTypes.TextFlagCapSentences | InputTypes.ClassText; //TODO check if correct
+            container.AddView(nameEditText);
+
+            var attentionEditText = new AppCompatEditText(Context)
+            {
+                LayoutParameters = new LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent)
+            };
+            attentionEditText.SetHint(Resource.String.edit_shortcode_attention);
+            attentionEditText.SetTextAppearanceCompat(Context, Resource.Style.fontPrimary);
+            attentionEditText.InputType = InputTypes.TextFlagCapSentences | InputTypes.ClassText;
+            container.AddView(attentionEditText);
+
+            if (row != null)
+            {
+                da = row.GetContent();
+                emailEditText.Text = da.Address;
+                nameEditText.Text = da.Name;
+                attentionEditText.Text = da.FullAttention; //TODO we need to know if it gets modified
+            }
+
+            Func<bool> isContentValid = () =>
+            {
+                var isValid = Validator.IsEmailValid(emailEditText.Text);
+
+                if (!isValid)
+                {
+                    emailEditText.Error = Context.GetString(Resource.String.edit_contact_invalid_email);
+                    emailEditText.TextChanged -= EmailEditText_TextChanged;
+                    emailEditText.TextChanged += EmailEditText_TextChanged;
+                }
+
+                return isValid;
+            };
+
+            if (await Dialogs.ShowCustomViewDialogWithValidityAsync(Context, Resource.String.edit_contact_email, container, isContentValid) == true)
+            {
+                da = da ?? new DocumentAddress();
+                da.Type = CommunicationAddressType.Email;
+                da.AddressType = addressType;
+                da.Address = emailEditText.Text;
+                da.Attention = attentionEditText.Text;
+                da.Name = nameEditText.Text;
+
+                if (row == null)
+                {
+                    AddRow(da);
+                    Shortcode.Addresses.Add(da);
+                }
+                else
+                {
+                    row.SetContent(da);
+                }
+            }
+        }
+
+        void EmailEditText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var emailEditText = (AppCompatEditText)sender;
+            emailEditText.Error = !Validator.IsEmailValid(emailEditText.Text)
+                ? Context.GetString(Resource.String.edit_contact_invalid_email) : null;
+        }
+
+        protected class EntryRow : Row
+        {
+            readonly AppCompatEditText emailEditText;
+
+            public EntryRow(Context context, EntryView entryView)
+                : base(context, entryView)
+            {
+                emailEditText = new AppCompatEditText(context)
+                {
+                    LayoutParameters = new LayoutParams(0, ViewGroup.LayoutParams.WrapContent, 1.0f),
+                    KeyListener = null,
+                    Focusable = false,
+                };
+                emailEditText.SetTextAppearanceCompat(Context, Resource.Style.fontPrimary);
+                emailEditText.SetHint(Resource.String.edit_contact_address);
+                emailEditText.Click += EmailEditText_Click;
+                Layout.AddView(emailEditText, 0);
+            }
+
+            void EmailEditText_Click(object sender, EventArgs e)
+            {
+                ((EntryView)ParentView).CreateDialog(this);
+            }
+
+            override public void UpdateRow()
+            {
+                emailEditText.Text = Content.Address;
+                emailEditText.Error = !Validator.IsEmailValid(Content.Address) ? Context.GetString(Resource.String.edit_contact_invalid_email) : null;
+            }
         }
     }
 }
