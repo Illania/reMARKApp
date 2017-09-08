e111254dd4ebdcac5e80b223f7e52292d1a2a54f
Mathias Dahl Thomsen <mdt@nordic-it.com>
2017-09-08 16:50:33 +0200

    receiver/service?

---
+++ Mark5.Mobile.Droid/Service/CallStateBroadcastReceiver.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Telephony;
+using Mark5.Mobile.Common.Manager;
+using Mark5.Mobile.Common.Model;
+
+namespace Mark5.Mobile.Droid.Service
+{
+    [BroadcastReceiver()]
+    [IntentFilter(new[] { "android.intent.action.PHONE_STATE" })]
+    public class CallStateBroadcastReceiver : BroadcastReceiver
+    {
+        IContactsManager contactsManager;
+
+        public CallStateBroadcastReceiver(IContactsManager cm)
+        {
+            this.contactsManager = cm;
+        }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (intent.Extras != null)
+            {
+                
+                var tm = (TelephonyManager)context.GetSystemService(Context.TelephonyService);
+                var phoneState = intent.GetStringExtra(TelephonyManager.ExtraState);
+                var incPhoneNumber = intent.GetStringExtra(TelephonyManager.ExtraIncomingNumber);
+                string contactPhoneNumber;
+
+                if (phoneState == TelephonyManager.ExtraStateRinging)
+                {
+                    contactPhoneNumber = contactsManager.GetContactPreviewsAsync(Folder.RootForModule(ModuleType.Contacts), sourceType: SourceType.Local).Result
+                                                 .Find(cp => cp.PrimaryAddress.Type.Equals(CommunicationAddressType.Phone) && cp.PrimaryAddressString.Equals(incPhoneNumber)).PrimaryAddress.Address;
+                    if(contactPhoneNumber != null)
+                    {
+                        //Change incoming call GUI?
+                    }
+                    
+                }
+
+            }
+        }
+    }
+}
---
+++ Mark5.Mobile.Droid/Service/IncomingCallService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Mark5.Mobile.Common.Manager;
+
+namespace Mark5.Mobile.Droid.Service
+{
+    [Service(IsolatedProcess = true, Exported = true, Name = "Mark5.Mobile.Droid.Service.IncomingCallService")]
+    public class IncomingCallService : Android.App.Service
+    {
+        CallStateBroadcastReceiver stateReceiver;
+
+        public IncomingCallService(IContactsManager cm)
+        {
+            stateReceiver = new CallStateBroadcastReceiver(cm);
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
