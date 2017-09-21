cf07d54b08c6b500b36468a444e03970866adf3a
Mathias Dahl Thomsen <mdt@nordic-it.com>
2017-09-21 16:42:51 +0200

    using

--- Mark5.Mobile.Droid/Ui/Activities/MainActivity.cs
+++ Mark5.Mobile.Droid/Ui/Activities/MainActivity.cs
@@ -17,7 +17,7 @@ using Android.Views;
 using Mark5.Mobile.Common;
 using Mark5.Mobile.Common.Manager;
 using Mark5.Mobile.Common.Model;
-using Mark5.Mobile.Common.Utilities;
+using Mark5.Mobile.Droid.Utilities;
 using Mark5.Mobile.Droid.Ui.Common;
 using Mark5.Mobile.Droid.Ui.Fragments;
 
