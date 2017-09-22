0237c32a8a55d59482cf3d765d95a2cb1ba107d3
Mathias Dahl Thomsen <mdt@nordic-it.com>
2017-09-22 10:06:47 +0200

    merge

---
+++ Mark5.Mobile.Droid/Service/CallActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Mark5.Mobile.Common.Model;
+
+namespace Mark5.Mobile.Droid.Service
+{
+    [Activity(Label = "CallActivity")]
+    public class CallActivity : Activity
+    {
+        Contact contact;
+
+        public static CallActivity CreateIntent(Context context, Contact contact)
+        {
+            var intent = new Intent(context, typeof(CallActivity));
+
+
+
+            return intent;
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
--- Mark5.Mobile.Droid/Service/CallStateBroadcastReceiver.cs
+++ Mark5.Mobile.Droid/Service/CallStateBroadcastReceiver.cs
@@ -12,33 +12,42 @@ namespace Mark5.Mobile.Droid.Service
     {
         IContactsManager contactsManager;
 
-        public CallStateBroadcastReceiver(IContactsManager cm)
+        public CallStateBroadcastReceiver(IContactsManager contactsManager)
         {
-            this.contactsManager = cm;
+            this.contactsManager = contactsManager;
         }
 
         public override void OnReceive(Context context, Intent intent)
         {
-            if (intent.Extras != null)
+            var tm = (TelephonyManager)context.GetSystemService(Context.TelephonyService);
+            var callListener = new IncomingCallListener(contactsManager);
+            tm.Listen(callListener, PhoneStateListenerFlags.CallState); 
+        }
+    }
+
+    class IncomingCallListener : PhoneStateListener
+    {
+        IContactsManager contactsManager;
+
+        public IncomingCallListener(IContactsManager contactsManager)
+        {
+            this.contactsManager = contactsManager;    
+        }
+
+        public override void OnCallStateChanged(CallState state, string incomingNumber)
+        {
+            base.OnCallStateChanged(state, incomingNumber);
+
+            if(state == CallState.Ringing)
             {
-                
-                var tm = (TelephonyManager)context.GetSystemService(Context.TelephonyService);
-                var phoneState = intent.GetStringExtra(TelephonyManager.ExtraState);
-                var incPhoneNumber = intent.GetStringExtra(TelephonyManager.ExtraIncomingNumber);
-                string contactPhoneNumber;
-
-                if (phoneState == TelephonyManager.ExtraStateRinging)
-                {
-                    contactPhoneNumber = contactsManager.GetContactPreviewsAsync(Folder.RootForModule(ModuleType.Contacts), sourceType: SourceType.Local).Result
-                                                 .Find(cp => cp.PrimaryAddress.Type.Equals(CommunicationAddressType.Phone) && cp.PrimaryAddressString.Equals(incPhoneNumber)).PrimaryAddress.Address;
-                    if(contactPhoneNumber != null)
-                    {
-                        //Change incoming call GUI?
-                    }
+                var contactPhoneNumber = contactsManager.GetContactPreviewsAsync(Folder.RootForModule(ModuleType.Contacts), sourceType: SourceType.Local).Result
+                                                        .Find(cp => cp.PrimaryAddress.Type.Equals(CommunicationAddressType.Phone) && cp.PrimaryAddressString.Equals(incomingNumber)).PrimaryAddress.Address;
+                if (contactPhoneNumber != null)
                     
-                }
-
+                    
             }
+
+
         }
     }
 }
