06459974c6386d62dd71017feb2b20a8f06642ed
Ferdinando Papale <fp@nordic-it.com>
2017-09-27 16:39:58 +0200

    Added Android analyitic logger

---
+++ Mark5.Mobile.Common/Model/AnalyticsEvents/AnalyticsEvent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace Mark5.Mobile.Common.Model.AnalyticsEvents
+{
+    public abstract class AnalyticsEvent
+    {
+        public abstract string Name { get; }
+        public List<AnalyticsParameter> Parameters { get; }
+    }
+
+    public abstract class AnalyticsParameter
+    {
+        public string Name { get; protected set; }
+    }
+
+    public abstract class StringAnalyticsParameter : AnalyticsParameter
+    {
+        public string Value { get; private set; }
+
+        protected StringAnalyticsParameter(string name, string stringValue)
+        {
+            Name = name;
+            Value = stringValue;
+        }
+    }
+
+    public abstract class NumberAnalyticsParameter : AnalyticsParameter
+    {
+        public int Value { get; private set; }
+
+        protected NumberAnalyticsParameter(string name, int numberValue)
+        {
+            Name = name;
+            Value = numberValue;
+        }
+    }
+
+    public class OpenNotificationListEvent : AnalyticsEvent
+    {
+        public override string Name => "open_notification_list";
+    }
+
+    public class OpenSubfolderEvent : AnalyticsEvent
+    {
+        public override string Name => "open_subfolder";
+
+        ModuleType module;
+        FolderType type;
+
+        public OpenSubfolderEvent(ModuleType module, FolderType type)
+        {
+            this.module = module;
+            this.type = type;
+        }
+    }
+}
---
+++ Mark5.Mobile.Droid/Utilities/Analytics.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Android.OS;
+using Firebase.Analytics;
+using Mark5.Mobile.Common.Model.AnalyticsEvents;
+
+namespace Mark5.Mobile.Droid.Utilities
+{
+    public static class Analytics
+    {
+        static FirebaseAnalytics firebaseAnalytics;
+
+        public static void Initialize(FirebaseAnalytics fa)
+        {
+            firebaseAnalytics = fa;
+        }
+
+        public static void LogEvent(AnalyticsEvent analyticsEvent)
+        {
+            Bundle bundle = null;
+            if (analyticsEvent.Parameters.Any())
+            {
+                bundle = new Bundle();
+                foreach (var parameter in analyticsEvent.Parameters)
+                {
+                    if (parameter is StringAnalyticsParameter stringParameter)
+                    {
+                        bundle.PutString(stringParameter.Name, stringParameter.Value);
+                    }
+                    else if (parameter is NumberAnalyticsParameter numberParameter)
+                    {
+                        bundle.PutLong(numberParameter.Name, numberParameter.Value);
+                    }
+                }
+            }
+            firebaseAnalytics.LogEvent(analyticsEvent.Name, bundle);
+        }
+    }
+}
