e29527336359f244be27fe9a9fa2d66f2c1ff289
Ferdinando Papale <fp@nordic-it.com>
2017-10-05 12:50:44 +0200

    Corrected crash because of null default outgoing line

--- Mark5.Mobile.IOS/Ui/ViewControllers/ComposeDocumentView/Subviews/LineView.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/ComposeDocumentView/Subviews/LineView.cs
@@ -108,7 +108,7 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentViews.Subviews
                     return Task.CompletedTask;
                 }
 
-                if (PreviousDocument.Lines.FirstOrDefault(l => l.Guid == defaultOutgoingLine.Guid) != null)
+                if (PreviousDocument.Lines.FirstOrDefault(l => l.Guid == defaultOutgoingLine?.Guid) != null)
                     SetLine(defaultOutgoingLine);
                 else
                 {
