842800764e85db71a099cadc951989c68b75ddcd
Mathias Dahl Thomsen <mdt@nordic-it.com>
2017-11-03 09:00:43 +0100

    shared db ext

--- CallOverlayExtension/SharedDatabase.cs
+++ CallOverlayExtension/SharedDatabase.cs
@@ -1,10 +1,102 @@
 ﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using SQLite;
+
 namespace CallOverlayExtension
 {
-    public class SharedDatabase
+    public static class SharedDatabase
     {
-        public SharedDatabase()
+        //Retrieves all contacts stored in the database in the shared container.
+
+        const string databaseFileName = "sharedcontacts.sqlite3";
+        const string databaseLockName = "sharedcontacts.lock";
+        const string appGroupId = "group.com.nordic-it.mark5.mobile.ios";
+
+        public static List<(string name, long number)> GetContactsFromSharedDatabase(string name, string number)
         {
+            List<ExtensionContact> dbContacts = null;
+
+            using (var containerUrl = NSFileManager.DefaultManager.GetContainerUrl(appGroupId))
+            {
+                var fullDatabaseUrl = containerUrl.Append(databaseFileName, false);
+               
+                using (var connection = new SQLiteConnection(fullDatabaseUrl.Path, SQLiteOpenFlags.FullMutex, true))
+                {
+                    try
+                    {
+                        LockDatabase();
+
+                        connection.RunInTransaction(() => {
+                            var commandString = $"select * "
+                                + $"from {nameof(ExtensionContact)} ";
+
+                            var cmd = connection.CreateCommand(commandString);
+                            var result = cmd.ExecuteQuery<ExtensionContact>();
+                            dbContacts = result;
+                        });
+                    }
+                    finally { UnlockDatabase(); }
+                }
+            }
+
+            return ProcessResult(dbContacts);
         }
+
+        static List<(string name, long number)> ProcessResult(List<ExtensionContact> dbResult)
+        {
+            return null;
+        }
+
+        static void LockDatabase()
+        {
+            using (var containerUrl = NSFileManager.DefaultManager.GetContainerUrl(appGroupId))
+            {
+                var fm = NSFileManager.DefaultManager;
+                var lockPath = containerUrl.Append(databaseLockName, false).Path;
+
+                if (fm.FileExists(lockPath))
+                    throw new Exception("The database is locked, unable to get lock.");
+
+                fm.CreateFile(lockPath, new NSData(), new NSFileAttributes());
+            }
+        }
+
+        static void UnlockDatabase()
+        {
+            using (var containerUrl = NSFileManager.DefaultManager.GetContainerUrl(appGroupId))
+            {
+                var fm = NSFileManager.DefaultManager;
+                var lockPath = containerUrl.Append(databaseLockName, false).Path;
+
+                if (fm.FileExists(lockPath))
+                {
+                    NSError error = new NSError();
+                    fm.Remove(lockPath, out error);
+                    if (error.LocalizedFailureReason != null)
+                    {
+                        throw new Exception("Error database lock file: " + error.LocalizedFailureReason);
+                    }
+                }
+                else
+                    throw new Exception("The database is locked, unable to get lock.");
+            }
+        }
+    }
+
+    [Table("ExtensionContact")]
+    public class ExtensionContact
+    {
+        [Column("Id")]
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [NotNull]
+        public string Name { get; set; }
+
+        [Column("Number")]
+        [NotNull]
+        public string Numbers { get; set; }
     }
 }
