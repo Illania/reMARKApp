a2c7a65a045b2d0443793ee932be99cc87d4ab97
Bartosz Cichecki <bgc@nordic-it.com>
2017-12-12 15:08:14 +0100

    M5APP-474

---
+++ Mark5.Mobile.Common/Utilities/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Mark5.Mobile.Common.Utilities.Extensions
+{
+    public static class StringExtensions
+    {
+
+        static readonly Dictionary<char, char> replaceCharacters = new Dictionary<char, char>
+        {
+            {'„','"' },
+            {'“','"' },
+            {'”','"' }
+        };
+
+        public static string SanitizeForSearch(this string str)
+        {
+            if (str == null)
+                return null;
+
+            var result = str;
+
+            foreach (var kv in replaceCharacters)
+                result = result.Replace(kv.Key, kv.Value);
+
+            return result;
+        }
+    }
+}
