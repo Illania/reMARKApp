5ae693d73056a3fab494f140f27564a2a4a682b0
Ferdinando Papale <fp@nordic-it.com>
2018-02-01 13:29:41 +0100

    Added comment

--- Mark5.Mobile.IOS/Ui/ViewControllers/ComposeDocumentView/ComposeDocumentViewController.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/ComposeDocumentView/ComposeDocumentViewController.cs
@@ -940,7 +940,7 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentView
 
                 ProcessTemplate(template, previousDocumentPreview);
 
-                var insertTemplateJs = File.ReadAllText(NSBundle.MainBundle.PathForResource("html/insertTemplate", "js"));
+                var insertTemplateJs = File.ReadAllText(NSBundle.MainBundle.PathForResource("html/insertTemplate", "js")); //TODO need to test how template works with quotes
                 if (template.ContentType == ContentType.PlainText)
                 {
                     var templateText = Regex.Replace(template.Content, @"\r\n?|\n", "\\n", RegexOptions.Multiline);
