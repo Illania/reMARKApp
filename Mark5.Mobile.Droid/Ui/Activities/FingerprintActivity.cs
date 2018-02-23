9c66f8779c98294d1d0797646ebe1f47d9bc3d1a
Mathias Dahl Thomsen <mdt@nordic-it.com>
2018-02-23 14:42:53 +0100

    Removed fingerprintfragment

--- Mark5.Mobile.Droid/Ui/Activities/FingerprintActivity.cs
+++ Mark5.Mobile.Droid/Ui/Activities/FingerprintActivity.cs
@@ -2,17 +2,27 @@
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Support.V4.Hardware.Fingerprint;
+using Android.Support.V7.Widget;
 using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Utilities;
 using Mark5.Mobile.Droid.Ui.Common;
-using Mark5.Mobile.Droid.Ui.Fragments;
+using Mark5.Mobile.Droid.Utilities.Fingerprint;
 
 namespace Mark5.Mobile.Droid
 {
-    [Activity(ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity()]
     public class FingerprintActivity : BaseAppCompatActivity
     {
-        FingerprintFragment fpf;
-        string fpFragmentTag;
+        const string StateKey = "765aef8a-d625-40df-86a6-a037a4986d21";
+        FingerprintActivityState state;
+
+        AppCompatTextView instructions;
+
+        FingerprintCallback fingerprintCallback;
+        FingerprintManagerCompat fingerprintManager;
+        CryptoObjectUtility cryptoHelper;
+        Android.Support.V4.OS.CancellationSignal cancellationSignal;
 
         public static Intent CreateIntent(Context context)
         {
@@ -23,22 +33,79 @@ namespace Mark5.Mobile.Droid
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.base_layout);
+            SetContentView(Resource.Layout.fingerprint);
 
+            instructions = FindViewById<AppCompatTextView>(Resource.Id.fingerprint_instructions_textview);
+            
             if (savedInstanceState == null)
-            {              
-                var ft = SupportFragmentManager.BeginTransaction();
-                (fpf, fpFragmentTag) = FingerprintFragment.NewInstance();
-                ft.Replace(Resource.Id.fragment_container, fpf, fpFragmentTag);
-                ft.Commit();
+            {
+                state = new FingerprintActivityState();
+                fingerprintManager = FingerprintManagerCompat.From(this);
+                cryptoHelper = new CryptoObjectUtility();
+                cancellationSignal = new Android.Support.V4.OS.CancellationSignal();
+                fingerprintCallback = new FingerprintCallback(this);
+
+                setInstructions();
+
+                fingerprintManager.Authenticate(cryptoHelper.BuildCryptoObject(), 0, cancellationSignal, fingerprintCallback, null);
 
                 CommonConfig.Logger.Info($"Created {nameof(FingerprintActivity)}");
             }
             else
             {
-                fpf = (FingerprintFragment)SupportFragmentManager.FindFragmentById(Resource.Id.fragment_container);
+                setInstructions();
                 CommonConfig.Logger.Info($"Restored {nameof(FingerprintActivity)}");
             }
         }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+
+            state.CancellationSignal = cancellationSignal;
+            state.CryptoHelper = cryptoHelper;
+            state.FingerprintCallback = fingerprintCallback;
+            state.FingerprintManager = fingerprintManager;
+
+            outState.PutString(StateKey,Serializer.Serialize(state));
+        }
+
+        protected override void OnRestoreInstanceState(Bundle savedInstanceState)
+        {
+            base.OnRestoreInstanceState(savedInstanceState);
+
+            state = Serializer.Deserialize<FingerprintActivityState>(savedInstanceState.GetString(StateKey));
+
+            cancellationSignal = state.CancellationSignal;
+            cryptoHelper = state.CryptoHelper;
+            fingerprintCallback = state.FingerprintCallback;
+            fingerprintManager = state.FingerprintManager;
+        }
+
+        void setInstructions()
+        {
+            if(fingerprintManager != null && instructions != null)
+            {
+                if (!fingerprintManager.HasEnrolledFingerprints)
+                    instructions.Text = "No enrolled fingerprints.";
+                else
+                    instructions.Text = "Scan fingerprint.";
+            }
+        }
     }
+
+    #region State class
+
+    class FingerprintActivityState
+    {
+        public FingerprintCallback FingerprintCallback { get; set; }
+
+        public FingerprintManagerCompat FingerprintManager { get; set; }
+
+        public CryptoObjectUtility CryptoHelper { get; set; }
+
+        public Android.Support.V4.OS.CancellationSignal CancellationSignal { get; set; }
+    }
+
+    #endregion
 }
