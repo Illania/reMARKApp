b676c3c86e96becf21d38daa4cf01f313cd3310f
Ferdinando Papale <fp@nordic-it.com>
2018-03-06 12:49:57 +0100

    Corrections to icons

--- Mark5.Mobile.IOS/Ui/ViewControllers/ContactViewController.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/ContactViewController.cs
@@ -318,7 +318,7 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers
                 {
                     editButtonItem = new UIBarButtonItem
                     {
-                        Image = UIImage.FromBundle(Path.Combine("icons", "pencil")),
+                        Image = UIImage.FromBundle(Path.Combine("icons", "edit")),
                         Enabled = false
                     };
                     NavigationItem.SetRightBarButtonItem(editButtonItem, false);
