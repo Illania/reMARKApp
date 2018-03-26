3939f94bdac1e0b4a49296315ecf1fdec4025d74
Ferdinando Papale <fp@nordic-it.com>
2018-03-26 15:55:06 +0200

    Starting to implement UI

---
+++ Mark5.Mobile.Droid/UI/Fragments/FingerprintDialogFragment.cs
@@ -0,0 +1,43 @@
+﻿using Android.OS;
+using Android.App;
+using Android.Views;
+using Android.Support.V4.Hardware.Fingerprint;
+using Mark5.Mobile.Droid.Utilities.Fingerprint;
+
+namespace Mark5.Mobile.Droid.Ui.Fragments
+{
+    public class FingerprintDialogFragment : DialogFragment
+    {
+        FingerprintUiHelper fingerprintUiHelper;
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            RetainInstance = true;
+            SetStyle(DialogFragmentStyle.Normal, Android.Resource.Style.ThemeMaterialLightDialog);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.fingerprint_dialog_container, container, false);
+
+            var fingerprintManager = FingerprintManagerCompat.From(Context);
+            fingerprintUiHelper = new FingerprintUiHelper(fingerprintManager);
+
+            return view;
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            fingerprintUiHelper.StartListening();
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+            fingerprintUiHelper.StopListening();
+        }
+    }
+}
---
+++ Mark5.Mobile.Droid/Utilities/Fingerprint/FingerprintUiHelper.cs
@@ -0,0 +1,63 @@
+﻿using Android.Support.V4.Hardware.Fingerprint;
+using Android.Support.V4.OS;
+using Android.Widget;
+
+namespace Mark5.Mobile.Droid.Utilities.Fingerprint
+{
+    public class FingerprintUiHelper : FingerprintManagerCompat.AuthenticationCallback
+    {
+        readonly FingerprintManagerCompat fingerprintManager;
+        CancellationSignal cancellationSignal;
+        bool selfCancelled;
+        readonly ImageView fingerprintIcon;
+
+        public bool IsFingerprintAuthAvailable => fingerprintManager.IsHardwareDetected
+                                         && fingerprintManager.HasEnrolledFingerprints;
+
+        public FingerprintUiHelper(FingerprintManagerCompat fingerPrintManager) //TODO need to pass icon
+        {
+            this.fingerprintManager = fingerPrintManager;
+        }
+
+        public void StartListening()
+        {
+            if (!IsFingerprintAuthAvailable)
+                return;
+
+            cancellationSignal = new CancellationSignal();
+            selfCancelled = false;
+            fingerprintManager.Authenticate(null, 0, cancellationSignal, this, null);
+            fingerprintIcon.SetImageResource(Resource.Drawable.ic_fp_40px);
+        }
+
+        public void StopListening()
+        {
+            if (cancellationSignal != null)
+            {
+                selfCancelled = true;
+                cancellationSignal.Cancel();
+                cancellationSignal = null;
+            }
+        }
+
+        public override void OnAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result)
+        {
+
+        }
+
+        public override void OnAuthenticationFailed()
+        {
+
+        }
+
+        public override void OnAuthenticationError(int errMsgId, Java.Lang.ICharSequence errString) //Unrecoverable error, nothing todo except try again.
+        {
+
+        }
+
+        public override void OnAuthenticationHelp(int helpMsgId, Java.Lang.ICharSequence helpString) //Recoverable error, e.g. user swipes finger too fast.
+        {
+
+        }
+    }
+}
