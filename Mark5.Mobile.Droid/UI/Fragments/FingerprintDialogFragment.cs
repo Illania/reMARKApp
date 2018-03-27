7c4159ed5cb4327fd7fe52ad171aa93be418517d
Ferdinando Papale <fp@nordic-it.com>
2018-03-27 13:42:18 +0200

    Corrections

--- Mark5.Mobile.Droid/UI/Fragments/FingerprintDialogFragment.cs
+++ Mark5.Mobile.Droid/UI/Fragments/FingerprintDialogFragment.cs
@@ -1,7 +1,8 @@
-﻿using Android.OS;
-using Android.App;
-using Android.Views;
+﻿using Android.App;
+using Android.OS;
 using Android.Support.V4.Hardware.Fingerprint;
+using Android.Support.V7.Widget;
+using Android.Views;
 using Mark5.Mobile.Droid.Utilities.Fingerprint;
 
 namespace Mark5.Mobile.Droid.Ui.Fragments
@@ -20,10 +21,14 @@ namespace Mark5.Mobile.Droid.Ui.Fragments
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
+            Dialog.SetTitle(Resource.String.fingerprint_dialog_title);
+
             var view = inflater.Inflate(Resource.Layout.fingerprint_dialog_container, container, false);
+            var icon = view.FindViewById<AppCompatImageView>(Resource.Id.fingerprint_icon);
+            var status = view.FindViewById<AppCompatTextView>(Resource.Id.fingerprint_status);
 
             var fingerprintManager = FingerprintManagerCompat.From(Context);
-            fingerprintUiHelper = new FingerprintUiHelper(fingerprintManager);
+            fingerprintUiHelper = new FingerprintUiHelper(fingerprintManager, icon, status);
 
             return view;
         }
