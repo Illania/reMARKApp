126efbfa82a17ee597d3d01645e387fe34c5b09c
Ferdinando Papale <fp@nordic-it.com>
2018-04-05 12:52:43 +0200

    Corrections

--- Mark5.Mobile.Droid/Ui/Fragments/ComposeDocumentFragment.cs
+++ Mark5.Mobile.Droid/Ui/Fragments/ComposeDocumentFragment.cs
@@ -636,19 +636,14 @@ namespace Mark5.Mobile.Droid.Ui.Fragments
                     Activity?.Finish();
             };
 
-            Action dialogAction = () =>
-            {
-                Dialogs.ShowYesNoDialog(Context, Resource.String.invalid_emails_title, Resource.String.invalid_emails_content, sendAction, null);
-            };
-
             var allEmailsValid = new RecipientsView[] { toView, ccView, bccView }.All(rv => rv.AllEmailsValid);
 
             if (!allEmailsValid && subjectView.Empty)
-                Dialogs.ShowYesNoDialog(Context, Resource.String.subject_view_empty_title, Resource.String.subject_view_empty_content, dialogAction, null);
+                Dialogs.ShowYesNoDialog(Context, Resource.String.invalid_emails_and_subject_title, Resource.String.invalid_emails_and_subject_content, sendAction, null);
             else if (!allEmailsValid)
                 Dialogs.ShowYesNoDialog(Context, Resource.String.invalid_emails_title, Resource.String.invalid_emails_content, sendAction, null);
             else if (subjectView.Empty)
-                Dialogs.ShowYesNoDialog(Context, Resource.String.subject_view_empty_title, Resource.String.subject_view_empty_title, sendAction, null);
+                Dialogs.ShowYesNoDialog(Context, Resource.String.invalid_subject_title, Resource.String.invalid_subject_content, sendAction, null);
             else
                 sendAction();
         }
