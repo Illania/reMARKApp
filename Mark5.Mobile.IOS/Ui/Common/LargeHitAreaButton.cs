0ff117c4472e705cb9bf7b9a627a77552b0d8b2e
Ferdinando Papale <fp@nordic-it.com>
2018-05-15 14:50:08 +0200

    Corrections regarding button hit area and clicked addresses

---
+++ Mark5.Mobile.IOS/Ui/Common/LargeHitAreaButton.cs
@@ -0,0 +1,16 @@
+﻿using CoreGraphics;
+using UIKit;
+
+namespace Mark5.Mobile.IOS.Ui.Common
+{
+    public class LargeHitAreaButton : UIButton
+    {
+        public int HitAreaMargin { get; set; } = 5;
+
+        public override bool PointInside(CGPoint point, UIEvent uievent)
+        {
+            var area = Bounds.Inset(-HitAreaMargin, -HitAreaMargin);
+            return area.Contains(point);
+        }
+    }
+}
