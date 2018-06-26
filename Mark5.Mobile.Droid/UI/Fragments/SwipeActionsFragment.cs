f4ab548f6b3198fc9541ea36cd4d7d253dbe1d03
Janis Bruno Ozolins <jbo@nordic-it.com>
2018-06-26 13:38:57 +0200

    swipe action fragment styled

---
+++ Mark5.Mobile.Droid/UI/Fragments/SwipeActionsFragment.cs
@@ -0,0 +1,38 @@
+﻿using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Mark5.Mobile.Common;
+
+using Mark5.Mobile.Droid.Ui.Common;
+
+namespace Mark5.Mobile.Droid.Ui.Fragments
+{
+    public class SwipeActionsFragment : BaseFragment
+    {
+        
+        public static (SwipeActionsFragment fragment, string tag) NewInstance()
+        {
+            var fragment = new SwipeActionsFragment();
+            var tag = $"{nameof(SwipeActionsFragment)}";
+
+            return (fragment, tag);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            CommonConfig.Logger.Info($"Creating {nameof(SwipeActionsFragment)}");
+            var rootView = inflater.Inflate(Resource.Layout.swipe_actions_fragment, container, false);
+            return rootView;
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+
+            ((AppCompatActivity)Activity).SupportActionBar.Title = GetString(Resource.String.pref_swipe_options_title);
+            ((AppCompatActivity)Activity).SupportActionBar.Subtitle = null;
+
+            CommonConfig.Logger.Info($"Created {nameof(SwipeActionsFragment)}");
+        }
+    }
+}
