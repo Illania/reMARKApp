2fe6d3f830452b61f61a0c55c6cd1e0ef10efc5e
Mathias Dahl Thomsen <mdt@nordic-it.com>
2018-07-12 10:22:13 +0200

    PhoneNumberTableViewCell PriorityView

--- Mark5.Mobile.IOS/Ui/ViewControllers/ComposeDocumentView/Subviews/PriorityView.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/ComposeDocumentView/Subviews/PriorityView.cs
@@ -63,10 +63,10 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers.ComposeDocumentViews.Subviews
             ContainerView.AddSubview(selectedPriorityLabel);
             ContainerView.AddConstraints(new[]
             {
-                NSLayoutConstraint.Create(selectedPriorityLabel, NSLayoutAttribute.Top, NSLayoutRelation.Equal, ContainerView, NSLayoutAttribute.Top, 1f, VerticalMargin),
-                NSLayoutConstraint.Create(selectedPriorityLabel, NSLayoutAttribute.Left, NSLayoutRelation.Equal, label, NSLayoutAttribute.Right, 1f, InnerMargin),
-                NSLayoutConstraint.Create(selectedPriorityLabel, NSLayoutAttribute.Right, NSLayoutRelation.Equal, ContainerView, NSLayoutAttribute.Right, 1f, -HorizontalMargin),
-                NSLayoutConstraint.Create(selectedPriorityLabel, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, ContainerView, NSLayoutAttribute.Bottom, 1f, -VerticalMargin)
+                selectedPriorityLabel.TopAnchor.ConstraintEqualTo(ContainerView.TopAnchor, VerticalMargin),
+                selectedPriorityLabel.LeftAnchor.ConstraintEqualTo(label.RightAnchor, InnerMargin),
+                selectedPriorityLabel.RightAnchor.ConstraintEqualTo(ContainerView.RightAnchor, -HorizontalMargin),
+                selectedPriorityLabel.BottomAnchor.ConstraintEqualTo(ContainerView.BottomAnchor, -VerticalMargin)
             });
         }
 
