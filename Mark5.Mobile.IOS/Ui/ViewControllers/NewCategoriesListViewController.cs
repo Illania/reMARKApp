7b37d55223bdb8695aacb178baedc0cd6f0555cd
Janis Bruno Ozolins <jbo@nordic-it.com>
2018-08-22 14:44:03 +0200

    when searching show only available categories

--- Mark5.Mobile.IOS/Ui/ViewControllers/NewCategoriesListViewController.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/NewCategoriesListViewController.cs
@@ -229,14 +229,14 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers
                 {
                     ((DataSource)TableView.Source).SetItems(DataSource.Section.Selected, documentPreview.Categories);
                     var allAvailableCategories = await Managers.DocumentsManager.GetAllCategoriesAsync();
-                    searchResultCategories = allAvailableCategories.Where(x => x.Name.Contains(searchText) && !documentPreview.Categories.Any(y => y.Guid == x.Guid)).ToList();
+                    searchResultCategories = allAvailableCategories.Where(x => x.Name.Contains(searchText) && !categories.Any(y => y.Guid == x.Guid)).ToList();
                 }
 
                 if (BusinessEntityPreview is ContactPreview contactPreview)
                 {
                     ((DataSource)TableView.Source).SetItems(DataSource.Section.Selected, contactPreview.Categories);
                     var allAvailableCategories = await Managers.ContactsManager.GetAllCategoriesAsync();
-                    searchResultCategories = allAvailableCategories.Where(x => x.Name.Contains(searchText) && !contactPreview.Categories.Any(y => y.Guid == x.Guid)).ToList();
+                    searchResultCategories = allAvailableCategories.Where(x => x.Name.Contains(searchText) && !categories.Any(y => y.Guid == x.Guid)).ToList();
                 }
 
                 if (cancellationToken.IsCancellationRequested)
