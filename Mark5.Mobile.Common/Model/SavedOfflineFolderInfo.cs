cd56f8294c91692b0ec69a014ad66a5626cfdcdc
Janis Bruno Ozolins <jbo@nordic-it.com>
2018-10-23 12:13:25 +0200

    Minor corrections

--- Mark5.Mobile.Common/Model/SavedOfflineFolderInfo.cs
+++ Mark5.Mobile.Common/Model/SavedOfflineFolderInfo.cs
@@ -1,29 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Mark5.Mobile.Common.Model
 {
-    public class SavedOfflineFolderInfo : IEqualityComparer<SavedOfflineFolderInfo>
+    public class SavedOfflineFolderInfo
     {
         public int FolderId { get; set; } = -1;
         public Guid FolderGuid { get; set; } = Guid.Empty;
         public string FolderName { get; set; }
         public ModuleType Module { get; set; }
         public long LastDownloaded { get; set; } = -1;
-
-        public bool Equals(SavedOfflineFolderInfo x, SavedOfflineFolderInfo y)
-        {
-            if (x is null || y is null)
-                return false;
-            
-            return x.FolderGuid == y.FolderGuid;
-        }
-
-        public int GetHashCode(SavedOfflineFolderInfo folder)
-        {
-            if (folder is null) return 0;
-            int hashNumf = folder.FolderGuid == null ? 0 : folder.FolderGuid.GetHashCode();
-            return hashNumf;
-        }
     }
 }
