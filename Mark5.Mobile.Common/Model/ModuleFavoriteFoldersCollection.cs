058689d1023b3420d9cebf3a1aa2130505f13e23
Ferdinando Papale <fp@nordic-it.com>
2019-01-17 10:50:31 +0100

    Renaming

--- Mark5.Mobile.Common/Model/ModuleFavoriteFoldersCollection.cs
+++ Mark5.Mobile.Common/Model/ModuleFavoriteFoldersCollection.cs
@@ -1,21 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Mark5.Mobile.Common.Model.Converters;
-using Mark5.ServiceReference.DataContract;
 
 namespace Mark5.Mobile.Common.Model
 {
     public class ModuleFavorites
     {
-        public List<ModuleFavorite> ModuleFavoritesList { get; set; }
+        public List<ModuleFavorite> ModuleFavoritesList { get; set; } = new List<ModuleFavorite>();
 
         public DateTime UpdatedAt { get; set; }
-
-        public ModuleFavorites()
-        {
-            ModuleFavoritesList = new List<ModuleFavorite>();
-        }
     }
 
     public class ModuleFavorite
