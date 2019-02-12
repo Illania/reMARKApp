92e71fa6d36582516f297587c5448bd07c5fe6f9
Mathias Dahl Thomsen <mdt@nordic-it.com>
2019-02-12 10:48:53 +0100

    Monitor to replace ReachabilityBroadcastReceiver

---
+++ Mark5.Mobile.Droid/Service/ReachabilityMonitor.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Android.Content;
+using Android.Net;
+using Mark5.Mobile.Common;
+using static Android.Net.ConnectivityManager;
+
+namespace Mark5.Mobile.Droid.Service
+{
+    public class ReachabilityMonitor : NetworkCallback
+    {
+        readonly NetworkRequest networkRequest;
+
+        bool registered;
+
+        public ReachabilityMonitor()
+        {
+            networkRequest = new NetworkRequest.Builder().AddTransportType(TransportType.Cellular).AddTransportType(TransportType.Wifi).Build();
+        }
+
+        public void Register(Context context)
+        {
+            if (registered)
+                return;
+
+            registered = true;
+
+            ConnectivityManager cm = (ConnectivityManager)context.GetSystemService(Context.ConnectivityService);
+            cm.RegisterNetworkCallback(networkRequest, this);
+        }
+
+        public void Unregister(Context context)
+        {
+            if (!registered)
+                return;
+
+            registered = false;
+
+            ConnectivityManager cm = (ConnectivityManager)context.GetSystemService(Context.ConnectivityService);
+            cm.UnregisterNetworkCallback(this);
+        }
+
+        public override void OnAvailable(Network network)
+        {
+            base.OnAvailable(network);
+
+            CommonConfig.Logger.Info("Connectivity changed");
+
+            //Task.Run(() => CommonConfig.Reachability.Refresh());
+            CommonConfig.Reachability.Refresh();
+        }
+    }
+}
