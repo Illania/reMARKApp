205c9667406c22f763b2e50ce1414d3837022a15
Ferdinando Papale <fp@nordic-it.com>
2019-03-12 12:58:37 +0100

    Correction to actions and link

--- Mark5.Mobile.IOS/Ui/ViewControllers/DocumentView/DocumentViewController.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/DocumentView/DocumentViewController.cs
@@ -902,8 +902,8 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers
         {
             var actionLinksListString = new string[]
             {
-                Localization.GetString("actions"),
-                Localization.GetString("links")
+                Localization.GetString("history"),
+                Localization.GetString("overview")
             };
 
             var result = await Dialogs.ShowListActionSheetAsync(this, actionLinksListString, userActionsButton);
