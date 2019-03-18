477358a6dd157f9c5b127e31f0c7dad5043992ab
Ferdinando Papale <fp@nordic-it.com>
2019-03-18 13:57:14 +0100

    Added base presenters

---
+++ Mark5.Mobile.Common/Presenters/BasePresenter.cs
@@ -0,0 +1,27 @@
+﻿namespace Mark5.Mobile.Common.Presenters
+{
+    public abstract class BasePresenter<V> : IPresenter<V> where V : IView
+    {
+        protected V view;
+
+        public void Init(V view)
+        {
+            this.view = view;
+        }
+
+        public abstract void Start();
+        public abstract void Stop();
+    }
+
+    public interface IPresenter<V> where V : IView
+    {
+        void Init(V view);
+        void Start();
+        void Stop();
+    }
+
+    public interface IView
+    {
+
+    }
+}
---
+++ Mark5.Mobile.Common/Presenters/CalendarModule/CalendarPresenter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Mark5.Mobile.Common.Presenters.CalendarModule
+{
+    public class CalendarPresenter : BasePresenter<ICalendarView>, ICalendarPresenter
+    {
+        public void ClickAppointment()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class CalendarAppointmentViewModel
+    {
+
+    }
+
+    public interface ICalendarPresenter : IPresenter<ICalendarView>
+    {
+        void ClickAppointment();
+
+    }
+
+    public interface ICalendarView : IView
+    {
+        void ShowLoading();
+        void StopLoading();
+        void ShowError();
+        void UpdateAppointments(IEnumerable<CalendarAppointmentViewModel> caViewModels);
+    }
+}
