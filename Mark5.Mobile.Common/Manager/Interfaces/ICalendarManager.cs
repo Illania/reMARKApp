b8bebdfa7aa215e0cd0d2f9dd6791d720cc1e3b4
Ferdinando Papale <fp@nordic-it.com>
2019-05-20 09:38:12 +0200

    Better grouping of files

--- Mark5.Mobile.Common/Manager/Interfaces/ICalendarManager.cs
+++ Mark5.Mobile.Common/Manager/Interfaces/ICalendarManager.cs
@@ -23,6 +23,7 @@ namespace Mark5.Mobile.Common.Manager
     public interface IAppointmentsCache
     {
         event EventHandler<AppointmentsRetrievedEventArgs> AppointmentRetrieved;
+        event EventHandler<AppointmentsRetrievedEventArgs> RetrievalError;
 
         void GetAppointments(List<int> calendarIds, DateTime startDate, DateTime endDate);
 
