1c559a5484f6f2bdf2d9e99f0131850303d6e796
Ferdinando Papale <fp@nordic-it.com>
2020-07-22 14:49:03 +0200

    Corrections to authenticator

--- Mark5.Mobile.Common/MicrosoftAuthenticator/MicrosoftAuthService.cs
+++ Mark5.Mobile.Common/MicrosoftAuthenticator/MicrosoftAuthService.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Microsoft.Graph;
 using Microsoft.Identity.Client;
+using Newtonsoft.Json;
 
 namespace Mark5.Mobile.Common.MicrosoftAuthenticator
 {
-    public class MicrosoftAuthService
+    public class MicrosoftAuthenticator
     {
         string ClientID = "ca4a3013-2f7f-4733-aa6c-126c8d34216f";
         string RedirectURI = "msauth.com.nordic-it.mark5.mobile.ios://auth"; //TODO it is different for Android
         string[] Scopes = { "User.Read" };
 
-        GraphServiceClient GraphClient;
         IPublicClientApplication pca;
+        string accessToken;
         IAccount account;
 
-        public MicrosoftAuthService()
+        private static string GraphApiUrl = "https://graph.microsoft.com/v1.0";
+        private static string GraphCurrentUserUrl = $"{GraphApiUrl}/me";
+
+        public MicrosoftAuthenticator()
         {
             pca = PublicClientApplicationBuilder.Create(ClientID)
                                     .WithRedirectUri(RedirectURI)
@@ -25,66 +29,65 @@ namespace Mark5.Mobile.Common.MicrosoftAuthenticator
                                     .Build();
         }
 
-        public async Task DoAuthentication(object parentWindow)
+        public async Task Authenticate(object parentWindow, bool forceInteractive = false)
         {
-            var token = string.Empty;
+            if (account != null && string.IsNullOrEmpty(accessToken))
+                return;
+
+            AuthenticationResult authResult = null;
             var accounts = await pca.GetAccountsAsync();
-            try
+
+            if (!forceInteractive)
             {
                 try
                 {
-
                     account = accounts.FirstOrDefault();
-                    var authResult = await pca.AcquireTokenSilent(Scopes, account).ExecuteAsync();
-                    token = authResult.AccessToken;
+                    authResult = await pca.AcquireTokenSilent(Scopes, account).ExecuteAsync();
                 }
                 catch (MsalUiRequiredException)
                 {
-                    // The user was not already connected.
-                    try
-                    {
-                        var authResult = await this.pca.AcquireTokenInteractive(Scopes)
-                                                    .WithParentActivityOrWindow(parentWindow)
-                                                    .ExecuteAsync();
-
-                        if (authResult != null)
-                        {
-                            token = authResult.AccessToken;
-                            account = authResult.Account;
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        //TODO need to do something here
-                    }
+                    //The login needs to be interactive
                 }
             }
-            catch (Exception)
-            {
-                //TODO need to do something here
-            }
-            //return token;
 
+            if (account == null)
+            {
+                // The user was not already connected.
+                authResult = await pca.AcquireTokenInteractive(Scopes)
+                                           .WithParentActivityOrWindow(parentWindow)
+                                           .ExecuteAsync();
+            }
+
+            if (authResult != null)
+            {
+                accessToken = authResult.AccessToken;
+                account = authResult.Account;
+            }
         }
 
-        private async Task InitializeGraphClientAsync()
+        public async Task<MicrosoftUser> GetCurrentUser()
         {
-            try
-            {
-                // Initialize Graph client
-                GraphClient = new GraphServiceClient(new DelegateAuthenticationProvider(
-                    async (requestMessage) =>
-                    {
-                        var result = await pca.AcquireTokenSilent(Scopes, account)
-                            .ExecuteAsync();
+            using var client = new HttpClient();
+            var message = new HttpRequestMessage(HttpMethod.Get, GraphCurrentUserUrl);
+            message.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+            HttpResponseMessage response = await client.SendAsync(message);
+            MicrosoftUser currentUser = null;
 
-                        requestMessage.Headers.Authorization =
-                            new AuthenticationHeaderValue("Bearer", result.AccessToken);
-                    }));
-            }
-            catch (Exception ex)
+            if (response.IsSuccessStatusCode)
             {
+                string json = await response.Content.ReadAsStringAsync();
+                currentUser = JsonConvert.DeserializeObject<MicrosoftUser>(json);
             }
+
+            return currentUser;
         }
+
+        public class MicrosoftUser
+        {
+            public string Id { get; set; }
+            public string DisplayName { get; set; }
+            public string Mail { get; set; }
+        }
+
     }
 }
