be7e8685b867b26dd4117dddac93b73849c645d6
Anna Gulich <ag@nordic-it.com>
2020-09-09 13:35:11 +0300

    added missing ui items

--- Mark5.Mobile.Droid/Ui/Fragments/DocumentsListFragment.cs
+++ Mark5.Mobile.Droid/Ui/Fragments/DocumentsListFragment.cs
@@ -1825,8 +1825,8 @@ namespace Mark5.Mobile.Droid.Ui.Fragments
                 previewTextView = itemView.FindViewById<AppCompatTextView>(Resource.Id.list_item_document_external_preview);
                 categoriesLayout = itemView.FindViewById<LinearLayoutCompat>(Resource.Id.list_item_document_external_categories);
                 commentImageView = itemView.FindViewById<AppCompatImageView>(Resource.Id.list_item_document_external_comment);
-                priorityHighImageView = itemView.FindViewById<AppCompatImageView>(Resource.Id.list_item_document_high_priority);
-                priorityLowImageView = itemView.FindViewById<AppCompatImageView>(Resource.Id.list_item_document_low_priority);
+                priorityHighImageView = itemView.FindViewById<AppCompatImageView>(Resource.Id.list_item_document_external_high_priority);
+                priorityLowImageView = itemView.FindViewById<AppCompatImageView>(Resource.Id.list_item_document_external_low_priority);
                 itemContent = itemView.FindViewById<LinearLayoutCompat>(Resource.Id.list_item_document_external_internal_layout);
                 selectedOverlay = itemView.FindViewById<View>(Resource.Id.selected_overlay);
                 swipedBackground = itemView.FindViewById<View>(Resource.Id.swiped_background);
