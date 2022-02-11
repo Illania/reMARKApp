a22f4db8635e2bc236efaae9e85dc0bf620aaad8
Anna Gulich <ag@nordic-it.com>
2022-02-11 11:26:13 +0300

    implementation for droid

---
+++ Mark5.Mobile.Droid/Ui/Fragments/LinkedEmailListFragment.cs
@@ -0,0 +1,308 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using FastScrollRecycler;
+using Mark5.Mobile.Common;
+using Mark5.Mobile.Common.Extensions;
+using Mark5.Mobile.Common.Model;
+using Mark5.Mobile.Common.Utilities;
+using Mark5.Mobile.Droid.Ui.Activities;
+using Mark5.Mobile.Droid.Ui.Common;
+
+namespace Mark5.Mobile.Droid.Ui.Fragments
+{
+    public class LinkedEmailListFragment : BaseFragment, IMenuItemOnActionExpandListener, SearchView.IOnQueryTextListener
+    {
+        readonly Handler searchHandler = new Handler();
+
+        public Folder Folder { get; set; }
+        public Contact Contact { get; set; }
+        public ContactPreview ContactPreview { get; set; }
+
+        RecyclerView recyclerView;
+        PhonebookContactsListAdapter adapter;
+        PhonebookContactsListAdapter searchAdapter;
+        SearchView searchView;
+        IMenu menu;
+
+        public static (LinkedEmailListFragment fragment, string tag) NewInstance()
+        {
+            var fragment = new LinkedEmailListFragment ListFragment();
+            var tag = $"{nameof(LinkedEmailListFragment ListFragment)}";
+
+            return (fragment, tag);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            CommonConfig.Logger.Info($"Creating {nameof(LinkedEmailListFragment)}");
+
+            var rootView = inflater.Inflate(Resource.Layout.list, container, false);
+
+            var emptyView = rootView.FindViewById<AppCompatTextView>(Resource.Id.empty_view);
+            emptyView.SetText(Resource.String.no_phonebook);
+
+            var refreshLayout = rootView.FindViewById<SwipeRefreshLayout>(Resource.Id.swipe_refresh_layout);
+            refreshLayout.Enabled = false;
+
+            recyclerView = rootView.FindViewById<RecyclerView>(Resource.Id.recycler_view);
+            recyclerView.SetLayoutManager(new LinearLayoutManager(Activity));
+            recyclerView.AddItemDecoration(new DividerItemDecorator(Activity));
+
+            adapter = new PhonebookContactsListAdapter();
+            adapter.ItemClicked += Adapter_ItemClicked;
+            adapter.RegisterAdapterDataObserver(new LambdaEmptyAdapterObserver(() =>
+            {
+                if (recyclerView.GetAdapter() != adapter)
+                    return;
+
+                emptyView.Visibility = adapter.ItemCount < 1 ? ViewStates.Visible : ViewStates.Gone;
+                recyclerView.Visibility = adapter.ItemCount > 0 ? ViewStates.Visible : ViewStates.Gone;
+            }));
+            recyclerView.SetAdapter(adapter);
+
+            searchAdapter = new LinkedEmailListAdapter();
+            searchAdapter.ItemClicked += Adapter_ItemClicked;
+
+            HasOptionsMenu = true;
+
+            return rootView;
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+
+            ((AppCompatActivity)Activity).SupportActionBar.Title = GetString(Resource.String.phonebook);
+            ((AppCompatActivity)Activity).SupportActionBar.Subtitle = null;
+
+            CommonConfig.Logger.Info($"Created {nameof(LinkedEmailListFragment)}");
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+
+            if (adapter.ItemCount < 1)
+            {
+                CommonConfig.Logger.Info($"Refreshing {nameof(PhonebookContactsListFragment)}");
+                RefreshView();
+            }
+        }
+
+        void RefreshView()
+        {
+            List<Recipient> contacts = null;
+
+            Task.Run(() =>
+              {
+                  contacts = CommonConfig.Phonebook.GetPhonebookContacts();
+              }).ContinueWith(async t =>
+               {
+                   if (t.IsFaulted)
+                   {
+                       var ex = t.Exception.InnerException;
+                       CommonConfig.Logger.Error($"Error while retrieving phonebook contacts", ex);
+                       await Dialogs.ShowErrorDialogAsync(Activity, ex);
+                   }
+
+                   if (contacts == null)
+                   {
+                       await Dialogs.ShowConfirmDialogAsync(Activity, Resource.String.phonebook_contacts_no_access_title,
+                                                            Resource.String.phonebook_contacts_no_access_content);
+                       Activity?.Finish();
+                   }
+                   else
+                   {
+                       adapter.SetItems(contacts.OrderBy(c => c.Name.SafeSubstring(0, 1)));
+                   }
+               }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        void Adapter_ItemClicked(object sender, Recipient r)
+        {
+            var intent = new Intent();
+            intent.PutExtra(PhonebookContactsListActivity.RecipientResultKey, Serializer.Serialize(r));
+            Activity.SetResult(Result.Ok, intent);
+            Activity?.Finish();
+        }
+
+        #region Options Menu / Filtering
+        public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
+        {
+            this.menu = menu;
+
+            inflater.Inflate(Resource.Menu.menu_main, menu);
+            var filterItem = menu.FindItem(Resource.Id.action_filter);
+            filterItem.SetOnActionExpandListener(this);
+
+            searchView = (SearchView)filterItem.ActionView;
+            searchView.QueryHint = GetString(Resource.String.filter);
+            searchView.SetOnQueryTextListener(this);
+        }
+
+        public bool OnMenuItemActionExpand(IMenuItem item)
+        {
+            if (item.ItemId == Resource.Id.action_filter)
+            {
+                recyclerView.SwapAdapter(searchAdapter, true);
+                (this as SearchView.IOnQueryTextListener).OnQueryTextChange(string.Empty);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool OnMenuItemActionCollapse(IMenuItem item)
+        {
+            if (item.ItemId == Resource.Id.action_filter)
+            {
+                searchHandler.RemoveCallbacksAndMessages(null);
+                searchAdapter.Clear();
+                recyclerView.SwapAdapter(adapter, true);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        bool SearchView.IOnQueryTextListener.OnQueryTextChange(string newText)
+        {
+            searchHandler.RemoveCallbacksAndMessages(null);
+            searchHandler.PostDelayed(() =>
+            {
+                if (string.IsNullOrWhiteSpace(newText))
+                    searchAdapter.ReplaceItems(adapter.Items);
+                else
+                    searchAdapter.ReplaceItems(adapter.Items.Where(recipient => MatchesQuery(recipient, newText)).ToList());
+            }, 500);
+
+            return false;
+        }
+
+        bool SearchView.IOnQueryTextListener.OnQueryTextSubmit(string query)
+        {
+            return false;
+        }
+
+        static bool MatchesQuery(Recipient recipient, string query)
+        {
+            if (recipient.Address?.ContainsCaseInsensitive(query) ?? false)
+                return true;
+
+            if (recipient.Name?.ContainsCaseInsensitive(query) ?? false)
+                return true;
+
+            if (recipient.AddressDescription?.ContainsCaseInsensitive(query) ?? false)
+                return true;
+
+            if (recipient.ContactDescription?.ContainsCaseInsensitive(query) ?? false)
+                return true;
+
+            if (recipient.ShortId?.ContainsCaseInsensitive(query) ?? false)
+                return true;
+
+            return false;
+        }
+
+        #endregion
+
+        class LinkedEmailListFragment ListAdapter : RecyclerView.Adapter, ISectionedAdapter
+        {
+            public override int ItemCount => Items.Count;
+            public List<Recipient> Items { get; } = new List<Recipient>();
+
+            public event EventHandler<Recipient> ItemClicked = delegate { };
+
+            public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+            {
+                var viewHolder = holder as LinkedEmailViewHolder;
+                var recipient = Items[position];
+
+                viewHolder.ItemView.SetOnClickListener(new ActionOnClickListener(() => ItemClicked(this, recipient)));
+
+                viewHolder.Address = recipient.Address;
+                viewHolder.Name = recipient.Name;
+            }
+
+            public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+            {
+                var itemView = LayoutInflater.FromContext(parent.Context).Inflate(Resource.Layout.list_item_recipients, parent, false);
+                return new LinkedEmailViewHolder(itemView);
+            }
+
+            public void SetItems(IEnumerable<Recipient> recipients)
+            {
+                Items.Clear();
+                Items.AddRange(recipients);
+
+                NotifyItemRangeInserted(0, Items.Count);
+            }
+
+            public void Clear()
+            {
+                var size = Items.Count;
+                Items.Clear();
+                NotifyItemRangeRemoved(0, size);
+            }
+
+            public void ReplaceItems(List<Recipient> items)
+            {
+                Clear();
+                AppendItems(items);
+            }
+
+            public void AppendItems(List<Recipient> items)
+            {
+                var count = Items.Count;
+                Items.AddRange(items);
+                NotifyItemRangeInserted(count, items.Count);
+            }
+
+            string ISectionedAdapter.GetSectionName(int position)
+            {
+                return Items[position].Name?.SafeSubstring(0, 1)?.ToUpper() ?? "";
+            }
+
+            class LinkedEmailListViewHolder : RecyclerView.ViewHolder
+            {
+                public string Address { set => addressTextView.Text = value; }
+
+                public string Name
+                {
+                    set
+                    {
+                        if (string.IsNullOrEmpty(value))
+                        {
+                            nameTextView.Visibility = ViewStates.Gone;
+                        }
+                        else
+                        {
+                            nameTextView.Visibility = ViewStates.Visible;
+                            nameTextView.Text = value;
+                        }
+                    }
+                }
+
+                readonly AppCompatTextView addressTextView;
+                readonly AppCompatTextView nameTextView;
+
+                public LinkedEmailListViewHolder(View itemView) : base(itemView)
+                {
+                    addressTextView = itemView.FindViewById<AppCompatTextView>(Resource.Id.list_item_recipients_address);
+                    nameTextView = itemView.FindViewById<AppCompatTextView>(Resource.Id.list_item_recipients_name);
+                }
+            }
+        }
+
+    }
+}
