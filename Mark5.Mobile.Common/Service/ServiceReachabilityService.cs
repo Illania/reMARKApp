d78f08a86782d0ad5c12f516b8b1aeea71bb823e
Viktor Mudrenko <vm@nordic-it.com>
2022-08-30 12:28:41 +0300

    Cleanup

--- Mark5.Mobile.Common/Service/ServiceReachabilityService.cs
+++ Mark5.Mobile.Common/Service/ServiceReachabilityService.cs
@@ -2,16 +2,14 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Mark5.Mobile.Common.Manager;
-using Mark5.Mobile.Common.Model;
 using Polly;
 
 namespace Mark5.Mobile.Common.Service
 {
     public class ServiceReachabilityService : AbstractService, IServiceReachabilityService
     {
-
         public ServiceReachabilityService() : base(3 * 1000, false) { }
-        private volatile bool _isReachable = false;
+        private volatile bool _isReachable;
 
         protected override async Task Work(CancellationToken ct)
         {
@@ -21,31 +19,29 @@ namespace Mark5.Mobile.Common.Service
             {
                 while (!ct.IsCancellationRequested)
                 {
-                    if (ci != null)
+                    if (ci == null) 
+                        continue;
+
+                    static async Task<bool> RetryServiceConnect()
                     {
-                        var url = $"{(ci.SslMode == SslMode.Off ? "http" : "https")}://{ci.Hostname}:{ci.Port}";
+                        var policyResult = await Policy
+                            .HandleResult<bool>(r => r == false)
+                            .WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(2))
+                            .ExecuteAsync(async () =>
+                            {
+                                var result = await CommonConfig.Reachability.CheckWithServiceConnection();
+                                return result;
+                            });
 
-                        static async Task<bool> RetryServiceConnect()
-                        {
-                            bool policyResult = await Policy
-                                .HandleResult<bool>(r => r == false)
-                                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(2))
-                                .ExecuteAsync(async () =>
-                                {
-                                    var result = await CommonConfig.Reachability.CheckWithServiceConnection();
-                                    return result;
-                                });
-
-                            return policyResult;
-                        }
-                   
-                        var isReachable = await RetryServiceConnect();
-
-                       if(isReachable != _isReachable)
-                            CommonConfig.Reachability.RefreshServiceReachability(isReachable);
-
-                        _isReachable = isReachable;
+                        return policyResult;
                     }
+                        
+                    var isReachable = await RetryServiceConnect();
+                    if (isReachable == _isReachable) 
+                        continue;
+                    
+                    CommonConfig.Reachability.RefreshServiceReachability(isReachable);
+                    _isReachable = isReachable;
                 }
             }
             catch (Exception ex)
@@ -54,10 +50,6 @@ namespace Mark5.Mobile.Common.Service
             }
 
             CommonConfig.Logger.Info("Stopped service reachability check task...");
-
         }
     }
 }
-
-
-
