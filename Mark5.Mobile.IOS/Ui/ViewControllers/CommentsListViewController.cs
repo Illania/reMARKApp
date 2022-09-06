d1aaf4a8a6b7d10b3467a9ff7d067c3abeebdd3e
Viktor Mudrenko <vm@nordic-it.com>
2022-09-06 16:02:52 +0300

    Example of code to print

--- Mark5.Mobile.IOS/Ui/ViewControllers/CommentsListViewController.cs
+++ Mark5.Mobile.IOS/Ui/ViewControllers/CommentsListViewController.cs
@@ -329,6 +329,120 @@ namespace Mark5.Mobile.IOS.Ui.ViewControllers
             {
                 dismissAction();
             }
+
+            PrintPdfFile();
+        }
+        /*public bool PrintImage(Stream img)
+        {
+            var data = NSData.FromStream(img);
+            var uiimage = UIImage.LoadFromData(data);
+
+            var printer = UIPrintInteractionController.SharedPrintController;
+
+            if (printer == null)
+            {
+                Console.WriteLine("Unable to print at this time.");
+            }
+            else
+            {
+
+                var printInfo = UIPrintInfo.PrintInfo;
+                printInfo.OutputType = UIPrintInfoOutputType.General;
+                printInfo.JobName = "Print Job Name";
+                printer.PrintInfo = printInfo;
+                printer.PrintingItem = uiimage;
+                printer.ShowsPageRange = true;
+
+                var handler = new UIPrintInteractionCompletionHandler((printInteractionController, completed, error) =>
+                {
+                    if (completed)
+                    {
+                        Console.WriteLine("Print Completed.");
+                    }
+                    else if (!completed && error != null)
+                    {
+                        Console.WriteLine("Error Printing.");
+                    }
+
+                });
+
+                CGRect frame = new CGRect();
+                frame.Size = uiimage.Size;
+
+                printer.Present(true, handler);
+            }
+
+            return true;
+        }
+
+        public bool PrintPdfFile(Stream file)
+        {
+            var printInfo = UIPrintInfo.PrintInfo;
+            printInfo.OutputType = UIPrintInfoOutputType.General;
+            printInfo.JobName = "Print PDF";
+
+            //Get the path of the MyDocuments folder
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            //Get the path of the Library folder within the MyDocuments folder
+            var library = Path.Combine(documents, "..", "Library");
+            //Create a new file with the input file name in the Library folder
+            var filepath = Path.Combine(library, "PrintSampleFile");
+
+            //Write the contents of the input file to the newly created file
+            using (MemoryStream tempStream = new MemoryStream())
+            {
+                file.Position = 0;
+                file.CopyTo(tempStream);
+                File.WriteAllBytes(filepath, tempStream.ToArray());
+            }
+
+            var printer = UIPrintInteractionController.SharedPrintController;
+            printInfo.OutputType = UIPrintInfoOutputType.General;
+
+            printer.PrintingItem = NSUrl.FromFilename(filepath);
+            printer.PrintInfo = printInfo;
+
+
+            printer.ShowsPageRange = true;
+
+            printer.Present(true, (handler, completed, err) => {
+                if (!completed && err != null)
+                {
+                    Console.WriteLine("error");
+                }
+            });
+            file.Dispose();
+            return true;
+        }
+    }*/
+
+
+        public void PrintPdfFile()
+        {
+            var printInfo = UIPrintInfo.PrintInfo;
+            printInfo.JobName = "My first Print Job";
+            printInfo.OutputType = UIPrintInfoOutputType.General;
+            var textFormatter = new UISimpleTextPrintFormatter("Once upon a time...")
+            {
+                StartPage = 0,
+                MaximumContentWidth = 6 * 72,
+                PerPageContentInsets = new UIEdgeInsets(72, 72, 72, 72),
+            };
+
+            var printer = UIPrintInteractionController.SharedPrintController;
+            printer.PrintInfo = printInfo;
+            printer.PrintFormatter = textFormatter;
+            printer.ShowsPageRange = true;
+            printer.Present(true, (handler, completed, error) =>
+            {
+                if (!completed && error != null)
+                {
+                    Console.WriteLine($"Error: {error.LocalizedDescription ?? ""}");
+                }
+            });
+
+            printInfo.Dispose();
+            textFormatter.Dispose();
         }
 
         #endregion
