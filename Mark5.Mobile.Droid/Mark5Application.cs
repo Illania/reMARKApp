fae4b9c521541ca9621de5275fcea04c4e465a5b
Viktor Mudrenko <vm@nordic-it.com>
2023-03-28 15:58:54 +0300

    Revert "test"
    This reverts commit bf0d2273ffeb500fd5672ee87e9211430dd24729.

--- Mark5.Mobile.Droid/Mark5Application.cs
+++ Mark5.Mobile.Droid/Mark5Application.cs
@@ -37,8 +37,6 @@ namespace Mark5.Mobile.Droid
         {
             ThreadPool.SetMinThreads(50, 50);
             ThreadPool.SetMaxThreads(100, 100);
-            var t = 0;
-            t++;
         }
 
         public override void OnCreate()
