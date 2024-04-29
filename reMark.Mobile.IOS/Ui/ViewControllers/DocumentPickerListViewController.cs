7a30594313e3f198250b5fc51f50205188426510
illania <ag@nordic-it.com>
2024-04-30 01:36:22 +0300

    brought back attach from folder for ios

---
+++ reMark.Mobile.IOS/Ui/ViewControllers/DocumentPickerListViewController.cs
@@ -0,0 +1,33 @@
+﻿using reMark.Mobile.Common;
+using reMark.Mobile.Common.Manager;
+using reMark.Mobile.IOS.Ui.Common;
+using reMark.Mobile.Common.Model;
+
+namespace reMark.Mobile.IOS.Ui.ViewControllers
+{
+    public class DocumentPickerListViewController : DocumentsListViewController
+    {
+        readonly TaskCompletionSource<Document> tcs = new TaskCompletionSource<Document>();
+        public Task<Document> Result => tcs.Task;
+
+        public override async void DocumentSelected(DocumentPreview documentPreview)
+        {
+            try
+            {
+                var doc = await Managers.DocumentsManager.GetDocumentAsync(Folder, documentPreview.Id);
+                tcs.SetResult(doc);
+                DismissViewController(true, null);
+            }
+            catch (Exception ex)
+            {
+                CommonConfig.Logger.Error($"Downloading document failed [folder.name={Folder?.Name}, folder.id={Folder?.Id}, documentPreview.Id={documentPreview.Id}]", ex);
+                await Dialogs.ShowErrorAlertAsync(this, ex);
+            }
+        }
+
+        protected override void InitializeNavigationBar()
+        {
+            //Not needed.
+        }
+    }
+}
